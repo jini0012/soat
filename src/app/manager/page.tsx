368a47f710f8ca3dafb78377bfbed670f7a42efa
chohee.hwang <manent556@gmail.com>
2025-02-11 00:39:52 +0900

    [add] 관리자메인페이지 배치

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/manager/Header";
+import Main from "@/components/manager/Main";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/components/manager/Main.tsx
@@ -0,0 +1,17 @@
+import ButtonWithIcon from "./ButtonWithIcon";
+
+export default function Main() {
+  return (
+    <main className="flex flex-col justify-center items-center">
+      <ButtonWithIcon
+        iconSrc="/images/icons/pen.svg"
+        label="나의 공연 등록 및 관리"
+      />
+      <ButtonWithIcon iconSrc="/images/icons/ticket.svg" label="티켓 검증" />
+      <ButtonWithIcon
+        iconSrc="/images/icons/people-black.svg"
+        label="관리자 정보 수정"
+      />
+    </main>
+  );
+}
