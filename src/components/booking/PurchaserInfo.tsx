cc75f173fa5d8f2e1d12b43cba44a213867792ad
김지훈 <kim@jihun.io>
2025-02-10 20:04:44 +0900

    [add] PurchaserInfo 컴포넌트에 BookSection 추가 및 구조 개선

--- src/components/booking/PurchaserInfo.tsx
+++ src/components/booking/PurchaserInfo.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button } from "../controls/Button";
 import { Checkbox, TextInput } from "../controls/Inputs";
 import { Select } from "../controls/Select";
+import BookSection from "./BookSection";
 
 export default function PurchaserInfo({
   setProcess,
@@ -24,74 +25,76 @@ export default function PurchaserInfo({
   const [purchaserDay, setPurchaserDay] = useState(today.getDate().toString());
 
   return (
-    <section className="flex flex-col w-full p-3 gap-y-3">
-      <h3 className="font-bold text-xl">2. 예매자 정보</h3>
-      <Checkbox onChange={setIsSameAsPurchaser} checked={isSameAsPurchaser}>
-        회원 정보와 같습니다.
-      </Checkbox>
-      <TextInput
-        label="이름"
-        type="text"
-        value={purchaserName}
-        onChange={setPurchaserName}
-      />
-      <div>
-        <label>생년월일</label>
-        <ul className="grid grid-cols-3 w-full gap-x-2">
-          <li>
-            <Select
-              value={purchaserYear}
-              onChange={setPurchaserYear}
-              options={Array.from({ length: 120 }, (_, i) => ({
-                value: `${today.getFullYear() - i}`,
-                label: `${today.getFullYear() - i}년`,
-              }))}
-            />
-          </li>
-          <li>
-            <Select
-              value={purchaserMonth}
-              onChange={setPurchaserMonth}
-              options={Array.from({ length: 12 }, (_, i) => ({
-                value: `${i + 1}`,
-                label: `${i + 1}월`,
-              }))}
-            />
-          </li>
-          <li>
-            <Select
-              value={purchaserDay}
-              onChange={setPurchaserDay}
-              options={Array.from(
-                {
-                  length: new Date(
-                    +purchaserYear,
-                    +purchaserMonth,
-                    0
-                  ).getDate(),
-                },
-                (_, i) => ({
+    <>
+      <BookSection>
+        <h3 className="font-bold text-xl">2. 예매자 정보</h3>
+        <Checkbox onChange={setIsSameAsPurchaser} checked={isSameAsPurchaser}>
+          회원 정보와 같습니다.
+        </Checkbox>
+        <TextInput
+          label="이름"
+          type="text"
+          value={purchaserName}
+          onChange={setPurchaserName}
+        />
+        <div>
+          <label>생년월일</label>
+          <ul className="grid grid-cols-3 w-full gap-x-2">
+            <li>
+              <Select
+                value={purchaserYear}
+                onChange={setPurchaserYear}
+                options={Array.from({ length: 120 }, (_, i) => ({
+                  value: `${today.getFullYear() - i}`,
+                  label: `${today.getFullYear() - i}년`,
+                }))}
+              />
+            </li>
+            <li>
+              <Select
+                value={purchaserMonth}
+                onChange={setPurchaserMonth}
+                options={Array.from({ length: 12 }, (_, i) => ({
                   value: `${i + 1}`,
-                  label: `${i + 1}일`,
-                })
-              )}
-            />
-          </li>
-        </ul>
-      </div>
-      <TextInput
-        label="휴대전화"
-        type="tel"
-        value={purchaserPhone}
-        onChange={setPurchaserPhone}
-      />
-      <TextInput
-        label="이메일"
-        type="email"
-        value={purchaserEmail}
-        onChange={setPurchaserEmail}
-      />
-      <ul className="grid grid-cols-2 gap-x-2 mt-12">
+                  label: `${i + 1}월`,
+                }))}
+              />
+            </li>
+            <li>
+              <Select
+                value={purchaserDay}
+                onChange={setPurchaserDay}
+                options={Array.from(
+                  {
+                    length: new Date(
+                      +purchaserYear,
+                      +purchaserMonth,
+                      0
+                    ).getDate(),
+                  },
+                  (_, i) => ({
+                    value: `${i + 1}`,
+                    label: `${i + 1}일`,
+                  })
+                )}
+              />
+            </li>
+          </ul>
+        </div>
+        <TextInput
+          label="휴대전화"
+          type="tel"
+          value={purchaserPhone}
+          onChange={setPurchaserPhone}
+        />
+        <TextInput
+          label="이메일"
+          type="email"
+          value={purchaserEmail}
+          onChange={setPurchaserEmail}
+        />
+      </BookSection>
+      <ul className="w-full grid grid-cols-2 gap-x-2 mt-12">
         <li>
           <Button size="full" onClick={() => setProcess("seat")}>
             뒤로 가기
@@ -101,12 +104,12 @@ export default function PurchaserInfo({
           <Button
             size="full"
             highlight
-            onClick={() => setProcess("purchaserInfo")}
+            onClick={() => setProcess("purchaseMethod")}
           >
             다음
           </Button>
         </li>
       </ul>
-    </section>
+    </>
   );
 }
