3a5213e7a357232cff692d67b6aced3f89504fa0
김지훈 <kim@jihun.io>
2025-02-10 20:04:35 +0900

    [add] 좌석 선택 컴포넌트에 BookSection 추가 및 구조 개선

--- src/components/booking/SeatSelection.tsx
+++ src/components/booking/SeatSelection.tsx
@@ -1,4 +1,6 @@
 import { Button } from "../controls/Button";
+import React from "react";
+import BookSection from "./BookSection";
 
 export default function SeatSelection({
   setProcess,
@@ -6,24 +8,26 @@ export default function SeatSelection({
   setProcess: (process: string) => void;
 }) {
   return (
-    <section className="flex flex-col w-full p-3 gap-y-3">
-      <h3 className="font-bold text-xl">1. 일자 및 좌석 선택</h3>
-      <ul className="flex gap-x-3">
-        <li>
-          <Button size="small">2월 10일</Button>
-        </li>
-        <li>
-          <Button size="small" highlight>
-            2월 11일
-          </Button>
-        </li>
-      </ul>
-      <div className="w-full h-[240px] bg-gray-200 rounded-md">
-        {/* 임시 */}
-      </div>
+    <>
+      <BookSection>
+        <h3 className="font-bold text-xl">1. 일자 및 좌석 선택</h3>
+        <ul className="flex gap-x-3">
+          <li>
+            <Button size="small">2월 10일</Button>
+          </li>
+          <li>
+            <Button size="small" highlight>
+              2월 11일
+            </Button>
+          </li>
+        </ul>
+        <div className="w-full h-[240px] bg-gray-200 rounded-md">
+          {/* 임시 */}
+        </div>
+      </BookSection>
       <Button size="full" highlight onClick={() => setProcess("purchaserInfo")}>
         다음
       </Button>
-    </section>
+    </>
   );
 }
