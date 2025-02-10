4c5137c8d6f3a466a5144ec64052dd95c6323d41
siwoon1602 <siwoon1602@gmail.com>
2025-02-10 20:08:28 +0900

    [add] login page 생성

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@/components/login/Header";
+import LoginContent from "./LoginContent";
+
+export default function login() {
+  return (
+    <>
+      <Header />
+      <LoginContent />
+    </>
+  );
+}
