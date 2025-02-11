c800024d50368c6bda33d76d983b13c19e70ee67
ddhsl <dnjs9199@naver.com>
2025-02-11 23:35:04 +0900

    [add] SearchResultItem 컴포넌트 추가 : 검색결과 아이템에 대한 컴포넌트 구현

---
+++ src/components/searchResult/SearchResultItem.tsx
@@ -0,0 +1,37 @@
+interface SearchResultItemProps {
+  imgUrl: string;
+  title: string;
+  team: string;
+  date: string;
+  commentCount: number;
+}
+
+export default function SearchResultItem({
+  imgUrl,
+  title,
+  team,
+  date,
+  commentCount,
+}: SearchResultItemProps) {
+  return (
+    <li>
+      <article className="py-[20px] flex items-center border-b border-b-gray-300">
+        <img src={imgUrl} alt="공연포스터" className="w-[84px] h-[118.8px]" />
+        <div className="ml-4">
+          <div className="flex gap-[7px] text-[8px]">
+            <div className="border border-flesh-500 px-1 py-[0.7px] text-flesh-500 rounded-sm mb-2">
+              판매예정
+            </div>
+            <p className="text-flesh-500">
+              D - <span>16</span>
+            </p>
+          </div>
+          <h3 className="font-semibold text-[15px]">{title}</h3>
+          <p className="text-[12px] font-light">{team}</p>
+          <p className="text-[12px] text-gray-400 font-light">{date}</p>
+          <p className="text-[12px] mt-3 font-light">한줄평({commentCount})</p>
+        </div>
+      </article>
+    </li>
+  );
+}
