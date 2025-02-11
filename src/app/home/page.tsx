9bfe2d5f5f4dd740a0afbdeda7fbffdb6337923a
siwoon1602 <siwoon1602@gmail.com>
2025-02-11 19:07:10 +0900

    [add] home의 page에 slidebanner 컴포넌트 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import Header from "@/components/home/Header";
 import Footer from "@/components/home/Footer";
+import SlideBanner from "@/components/home/SlideBanner";
 
 export default function main() {
   return (
     <>
       <Header />
+      <SlideBanner />
       <Footer />
     </>
   );
