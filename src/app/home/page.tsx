40bb6cc94d141a0c49f026f7710ca29e81b7b66a
siwoon1602 <siwoon1602@gmail.com>
2025-02-11 20:21:50 +0900

    [add] 현재상영중 컴포넌트 생성 및 스타일링,마크업 page에 연결

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import Header from "@/components/home/Header";
 import Footer from "@/components/home/Footer";
 import SlideBanner from "@/components/home/SlideBanner";
+import UpcomingShowsSection from "@/components/home/UpcomingShowsSection";
+import CurrentShowSection from "@/components/home/CurrentShowSection";
 
 export default function main() {
   return (
     <>
       <Header />
       <SlideBanner />
+      <CurrentShowSection />
+      <UpcomingShowsSection />
       <Footer />
     </>
   );
