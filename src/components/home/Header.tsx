ce48a66ab8851df72e894fde96067c9371639615
siwoon1602 <siwoon1602@gmail.com>
2025-02-11 15:09:23 +0900

    [edit] home 의 기본header 컴포넌트 마크업 및 스타일링, 기본로직 수정

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,14 +1,58 @@
 import React, { useState } from "react";
-import { TextInput } from "../controls/Inputs";
+import { SearchInput } from "../controls/Inputs";
+import Link from "next/link";
 
-export default function Hedaer() {
+export default function Header() {
   const [searchValue, setSearchValue] = useState<string>("");
+
+  const handleSearch = () => {
+    console.log("검색이되었습니다! ");
+    // 추후 api 호출 hook 추가예정
+  };
+
   return (
-    <header>
-      <h1>
-        <img src="images/icons/logo-temp.svg" alt="soat" />
-      </h1>
-      <TextInput value={searchValue} onChange={setSearchValue} />
+    <header className="px-[140px] py-[40px] flex items-center justify-between">
+      <div className="flex relative">
+        <h1>
+          <Link href="/">
+            <img
+              src="images/icons/logo-temp.svg"
+              alt="soat"
+              className="w-[93px]"
+            />
+          </Link>
+        </h1>
+        <SearchInput
+          label=""
+          placeholder="검색 할 내용을 입력해주세요!"
+          value={searchValue}
+          onChange={setSearchValue}
+          onSearch={handleSearch}
+          className="w-[300px]"
+          inputClassName="h-[43.47px] px-[20px]"
+        />
+        <img
+          src="images/icons/search-icon.svg"
+          alt="검색"
+          onClick={handleSearch}
+          className="w-[25px] absolute right-0 top-2 cursor-pointer"
+        />
+      </div>
+      <ul className="flex gap-[20px] ">
+        <li className="cursor-pointer">
+          <Link href="./login" className="text-black flex gap-[5px]">
+            <img src="/images/icons/mypage-login-icon.svg" alt="로그인" />
+            로그인
+          </Link>
+        </li>
+
+        <li className="cursor-pointer">
+          <Link href="./join" className="text-black flex gap-[5px]">
+            <img src="images/icons/join-icon.svg" alt="로그인" />
+            회원가입
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 }
