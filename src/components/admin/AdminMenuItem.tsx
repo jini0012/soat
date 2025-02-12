58ec983fab4b37036a92d938d5338c07f05901e4
ddhsl <dnjs9199@naver.com>
2025-02-12 18:19:12 +0900

    [edit] AdminMenuItem 수정 : sub menu 토글 기능 구현

---
+++ src/components/admin/AdminMenuItem.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
+interface AdminMenuLink {
+  text: string;
+  url: string;
+}
+
+interface AdminMenuItemProps {
+  title: string;
+  links: AdminMenuLink[];
+}
+
+export default function AdminMenuItem({ title, links }: AdminMenuItemProps) {
+  const [isOpenSubMenu, setIsOpenSubMenu] = useState(false); // sub menu 아이템 토글 상태를 관리
+
+  function handleToggleSubMenu() {
+    setIsOpenSubMenu((prev) => !prev);
+  }
+
+  return (
+    <li>
+      <h2
+        className="bg-chevron-down-dark bg-no-repeat bg-[center_right] font-medium"
+        onClick={handleToggleSubMenu} // 클릭 시 토글
+      >
+        {title}
+      </h2>
+      {isOpenSubMenu && (
+        <ul className="flex flex-col gap-1 mt-1">
+          {links.map((link) => (
+            <li key={link.url} className="pl-2 text-[12px]">
+              <Link href={link.url} className="no-underline text-inherit">
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+}
