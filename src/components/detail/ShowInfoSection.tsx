5608da69a69498517f926482189e02f96832afd8
siwoon1602 <siwoon1602@gmail.com>
2025-02-12 13:14:08 +0900

    [edit] showInfoSection 스타일링 마크업 완료

---
+++ src/components/detail/ShowInfoSection.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Button } from "../controls/Button";
+
+export default function ShowInfoSection() {
+  return (
+    <section className="flex px-24 py-16 gap-12">
+      <div className="w-80">
+        <img
+          src="images/derme.jpg"
+          alt="포스터"
+          className="w-80 h-[460px] rounded-xl"
+        />
+      </div>
+
+      <div className="flex-1 flex flex-col justify-between">
+        <div>
+          <div className="border-b border-gray-200 pb-4 mb-8">
+            <div className="flex justify-between items-center">
+              <div>
+                <h2 className="text-4xl font-bold mb-2">
+                  2025 주인님 단독공연
+                </h2>
+                <p className="text-xl text-gray-500">
+                  2025. 01. 21. - 2025. 01. 25
+                </p>
+              </div>
+              <img
+                src="images/icons/share-icon.svg"
+                alt="공유하기"
+                className="ml-4"
+              />
+            </div>
+          </div>
+
+          <ul className="space-y-6">
+            <li>
+              <p className="flex items-center gap-2 text-[24px] text-[#404040]">
+                <span className="font-semibold ">장소 :</span>
+                인천 어쩌구 멘토님 집 (8석)
+                <img
+                  src="images/icons/map-icon.svg"
+                  alt="지도보기"
+                  className="w-6 h-6 ml-2"
+                />
+              </p>
+            </li>
+            <li>
+              <p className="flex items-center gap-2 text-[24px]  text-[#404040]">
+                <span className="font-semibold">예매가 :</span>
+                1,000,000 원
+              </p>
+            </li>
+            <li>
+              <p className="flex items-center gap-2 text-[24px]  text-[#404040]">
+                <span className="font-semibold">카테고리 :</span>
+                코미디 (8석)
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <Button
+          highlight
+          className="text-white px-20 py-4 rounded-lg  text-[24px]"
+        >
+          예매하기
+        </Button>
+      </div>
+    </section>
+  );
+}
