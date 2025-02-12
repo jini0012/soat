91e30307bbae2a43d2edf507292f43647cdbd057
siwoon1602 <siwoon1602@gmail.com>
2025-02-12 13:35:30 +0900

    [add] 캡션,리뷰,공연정보 컴포넌트 생성

---
+++ src/components/detail/CautionArea.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CautionArea() {
+  return <div>CautionArea</div>;
+}
---
+++ src/components/detail/ReviewArea.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ReviewArea() {
+  return <div>ReviewArea</div>;
+}
