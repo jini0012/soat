f2502350ffed7a39f2d5e9586159c0dca44961c5
siwoon1602 <siwoon1602@gmail.com>
2025-02-12 13:13:37 +0900

    [add] showinfoSection 컴포넌트 page에 추가

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "@/components/home/Header";
 import Footer from "@/components/home/Footer";
+import ShowInfoSection from "@/components/detail/ShowInfoSection";
 
 export default function page() {
   return (
     <>
       <Header />
+      <ShowInfoSection />
       <Footer />
     </>
   );
