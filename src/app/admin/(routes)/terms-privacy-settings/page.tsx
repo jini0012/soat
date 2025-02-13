912bb94574d02b4c9d168c7fdf99290e4c31aa81
ddhsl <dnjs9199@naver.com>
2025-02-14 02:39:09 +0900

    [add] TermsAndPrivacySettingsPage 컴포넌트 추가 : 이용약관 및 개인정보처리방침 페이지 구현

---
+++ src/app/admin/(routes)/terms-privacy-settings/page.tsx
@@ -0,0 +1,19 @@
+import AdminHeader from "../../../../components/admin/AdminHeader";
+import AdminMain from "@/components/admin/AdminMain";
+import ListTitle from "@/components/admin/ListTitle";
+import SubTabDescription from "@/components/admin/SubTabDescription";
+import TermsPage from "@/components/admin/TermsPage";
+
+export default function TermsAndPrivacySettingsPage() {
+  return (
+    <>
+      <AdminHeader>컨텐츠 관리</AdminHeader>
+      <AdminMain>
+        <SubTabDescription>이용약관 / 개인정보처리방침 관리</SubTabDescription>
+        <p className="mt-5 mb-6 text-[12px]">이용약관 | 개인정보처리방침</p>
+        <ListTitle>이용약관</ListTitle>
+        <TermsPage />
+      </AdminMain>
+    </>
+  );
+}
