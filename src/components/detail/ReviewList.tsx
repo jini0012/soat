b987ad40a74a9db0af6451af5be0c7221151834c
김지훈 <kim@jihun.io>
2025-02-13 13:25:26 +0900

    [add] 리뷰 목록에 더미 데이터 추가 및 DetailPost 컴포넌트 렌더링

--- src/components/detail/ReviewList.tsx
+++ src/components/detail/ReviewList.tsx
@@ -1,5 +1,22 @@
 "use client";
 import React, { useState } from "react";
+import DetailPost from "./DetailPost";
+
+const demoData = [
+  {
+    ratings: 5,
+    date: "2021-10-10",
+    author: "집나온쿼카",
+    content: "사장님이 맛있고 음식이 친절해요!",
+  },
+  {
+    ratings: 4,
+    date: "2021-10-10",
+    author: "스테이크러버지니",
+    content:
+      "사장님 요리보단 바이올린만 하셔야 할듯....오히려 브로맨스가 제일 재밌었음 주인님 케미 미쳐요!",
+  },
+];
 
 export default function ReviewList() {
   const [isReviewAlign, setReviewAlign] = useState("LASTEST");
@@ -43,6 +60,18 @@ export default function ReviewList() {
           최신글순
         </li>
       </ul>
+      <section className="flex flex-col gap-y-3">
+        <h2 className="sr-only">한줄평 목록</h2>
+        {demoData.map((data) => (
+          <DetailPost
+            key={data.date + data.author}
+            ratings={data.ratings}
+            date={data.date}
+            author={data.author}
+            content={data.content}
+          />
+        ))}
+      </section>
     </div>
   );
 }
