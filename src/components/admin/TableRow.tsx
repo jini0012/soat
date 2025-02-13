7d3608b40be8860003352a44afabef25ac16f1cb
ddhsl <dnjs9199@naver.com>
2025-02-13 23:45:41 +0900

    [add] TableRow 컴포넌트 추가 : 페이지 관리자 공통 테이블 행 구현

---
+++ src/components/admin/TableRow.tsx
@@ -0,0 +1,22 @@
+interface TableRowProps<T> {
+  rowData: T;
+  headers: string[];
+  fieldMapping: { [key: string]: keyof T }; // 필드명과 헤더 텍스트를 매핑
+}
+
+export default function TableRow<T>({
+  rowData,
+  headers,
+  fieldMapping,
+}: TableRowProps<T>) {
+  return (
+    <tr className="text-[10px] border-b border-gray-300">
+      {headers.map((header) => (
+        <td key={header} className="text-center py-1">
+          {/* 필드명 기반으로 데이터 출력 ex: rowData["name"] */}
+          {rowData[fieldMapping[header]] as React.ReactNode}
+        </td>
+      ))}
+    </tr>
+  );
+}
