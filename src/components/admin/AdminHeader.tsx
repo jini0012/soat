8e7193529922af72236a511c20c23c888c870d24
ddhsl <dnjs9199@naver.com>
2025-02-14 16:22:19 +0900

    [add] 관리자 메뉴 닫기 기능 구현

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -8,32 +8,41 @@ interface AdminHeaderProps {
 }
 
 export default function AdminHeader({ children }: AdminHeaderProps) {
-  const [isOpenMenu, setIsOpenMenu] = useState(false); //관리자 메뉴 토글 상태 관리
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
 
-  function handleToggleMenu() {
-    setIsOpenMenu((prev) => !prev);
-    console.log("클릭성공");
+  function handleOpenMenu() {
+    setIsOpenMenu(true);
+  }
+
+  function handleCloseMenu() {
+    setIsOpenMenu(false);
   }
 
   return (
     <>
-      <header className="w-[100vw] h-[61px] flex items-center bg-flesh-500 text-white text-[22px] font-semibold">
-        <button onClick={handleToggleMenu} className="relative z-30">
+      {/* 딤드 배경 처리 */}
+      {isOpenMenu && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-70 z-30" // 딤드 배경의 z-index를 AdminMenu보다 낮게 설정
+          onClick={handleCloseMenu}
+        />
+      )}
+
+      <header className="w-[100vw] h-[61px] flex items-center bg-flesh-500 text-white text-[22px] font-semibold z-10 relative">
+        <button onClick={handleOpenMenu} className="relative z-30">
           <Hamburger className="fill-white" />
         </button>
 
         <h1 className="absolute w-[100%] text-center">{children}</h1>
       </header>
 
-      {isOpenMenu && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-70 z-20"
-          onClick={() => setIsOpenMenu(false)}
-        />
-      )}
-
-      <div className={`${isOpenMenu ? "block" : "hidden"} relative z-40`}>
-        <AdminMenu />
+      {/* AdminMenu */}
+      <div
+        className={`${
+          isOpenMenu ? "translate-x-0" : "-translate-x-full"
+        } fixed top-0 left-0 bottom-0 w-[240px] transition-transform duration-300 ease-in-out z-40`} // AdminMenu는 z-40으로 설정
+      >
+        <AdminMenu onClose={handleCloseMenu} />
       </div>
     </>
   );
--- src/components/admin/AdminMenu.tsx
+++ src/components/admin/AdminMenu.tsx
@@ -1,7 +1,12 @@
+"use client";
 import AdminMenuItem from "@/components/admin/AdminMenuItem";
 import Link from "next/link";
 
-export default function AdminMenu() {
+interface AdminMenuProps {
+  onClose: () => void;
+}
+
+export default function AdminMenu({ onClose }: AdminMenuProps) {
   const menuData = [
     {
       title: "회원관리",
@@ -31,7 +36,6 @@ export default function AdminMenu() {
       title: "컨텐츠관리",
       links: [
         { text: "메인 배너 등록 및 관리", url: "/admin/banner-list" },
-        // { text: "공지사항 등록 및 관리", url: "#" },
         {
           text: "이용약관 / 개인정보처리방침 관리",
           url: "terms-privacy-settings",
@@ -43,32 +47,31 @@ export default function AdminMenu() {
       links: [
         { text: "관리자 계정 관리", url: "/admin/site-admin-list" },
         { text: "서비스 점검 모드 설정", url: "/admin/maintenance-mode" },
-        // { text: "로그 관리", url: "#" },
       ],
     },
-    // {
-    //   title: "고객지원",
-    //   links: [
-    //     { text: "1:1 문의 관리", url: "#" },
-    //     { text: "FAQ 관리", url: "#" },
-    //   ],
-    // },
   ];
 
   return (
     <>
       <aside className="w-[240px] h-[100vh] bg-white fixed top-0 left-0 ">
-        <div className="w-[100%] h-[61px] bg-flesh-500 flex items-center justify-center">
-          <p className="text-[18px] font-semibold text-white">관리자님</p>
+        <div className="w-[100%] h-[61px] bg-flesh-400 flex items-center justify-center relative">
+          <p className="text-lg font-semibold text-white">관리자님</p>
+          <button onClick={onClose} className="absolute right-4">
+            <img
+              src="/images/icons/close-btn.svg"
+              alt="메뉴 닫기"
+              className="w-[17px]"
+            />
+          </button>
         </div>
         <div className="border-b-[1.5px] border-b-flesh-500 h-[35px] flex items-center">
-          <Link href="#" className="no-underline text-flesh-500  px-4">
+          <Link href="" className="no-underline text-flesh-500  px-4">
             So@ 바로가기
           </Link>
         </div>
         <nav className="flex flex-col items-start px-[6%]">
-          <p className="text-[12px] text-gray-500 mt-2 mb-2">사이트 관리</p>
-          <ul className="flex flex-col gap-4 w-[100%]">
+          <p className="text-xs text-gray-500 mt-2 mb-2">사이트 관리</p>
+          <ul className="flex flex-col gap-4 w-full">
             {menuData.map((item, index) => (
               <AdminMenuItem
                 key={index}
@@ -77,7 +80,7 @@ export default function AdminMenu() {
               />
             ))}
           </ul>
-          <button className="w-[100%] h-[33px] bg-flesh-500 text-white  mt-[38px]">
+          <button className="w-full h-[33px] rounded-md bg-flesh-400 text-white mt-[38px] hover:bg-flesh-500 active:bg-flesh-600">
             로그아웃
           </button>
         </nav>
