233d58c8578fa2afcd2dc5c68add82ace3aabc85
ddhsl <dnjs9199@naver.com>
2025-02-14 19:24:30 +0900

    [edit] 검색 결과 정렬 select option 값 추가

--- src/components/searchResult/SortFilter.tsx
+++ src/components/searchResult/SortFilter.tsx
@@ -1,3 +1,12 @@
 export default function SortFilter() {
-  return <button className="text-[12px]">낮은 가격순</button>;
+  return (
+    <>
+      <button className="text-xs">최근날짜순</button>
+      <div className="absolute top-[200px] rounded-md bg-white right-6 text-sm p-3 w-[88px] h-[105px] border border-gray-300 flex flex-col gap-2 shadow-lg ">
+        <div>최근날짜순</div>
+        <div>한줄평순</div>
+        <div>낮은가격순</div>
+      </div>
+    </>
+  );
 }
