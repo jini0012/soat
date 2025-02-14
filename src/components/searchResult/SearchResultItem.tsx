2e97948eb00a7a406e2c69bdfcba20ded705a500
ddhsl <dnjs9199@naver.com>
2025-02-14 19:21:51 +0900

    [edit] SearchResultItem 컴포넌트 수정 :  공연 포스터 이미지 크기 변경

--- src/components/searchResult/SearchResultItem.tsx
+++ src/components/searchResult/SearchResultItem.tsx
@@ -16,7 +16,7 @@ export default function SearchResultItem({
   return (
     <li>
       <article className="py-[20px] flex items-center border-b border-b-gray-300">
-        <img src={imgUrl} alt="공연포스터" className="w-[84px] h-[118.8px]" />
+        <img src={imgUrl} alt="공연포스터" className="w-[113px] h-[143px]" />
         <div className="ml-4">
           <div className="flex items-center  gap-[7px] text-[8px]">
             <div className="border border-flesh-500 px-1 py-[0.7px] text-flesh-500 rounded-sm">
@@ -26,10 +26,13 @@ export default function SearchResultItem({
               D - <span>16</span>
             </p>
           </div>
-          <h3 className="font-semibold text-[15px] mt-2">{title}</h3>
-          <p className="text-[12px] font-light">{venue}</p>
-          <p className="text-[12px] text-gray-400 font-light">{date}</p>
-          <p className="text-[12px] mt-3 font-light">한줄평({commentCount})</p>
+          <h3 className="font-semibold text-[15px] mt-1 mb-[0.8px]">{title}</h3>
+          <p className="text-xs font-light mb-[0.8px] ">{venue}</p>
+          <p className="text-xs text-gray-400 font-light">{date}</p>
+          <p className="text-xs mt-2 mb-2 font-light">한줄평({commentCount})</p>
+          <button className="w-[65px] h-[25px] rounded-md bg-flesh-400 hover:bg-flesh-500 active:bg-flesh-600 text-white font-medium text-xs">
+            예매하기
+          </button>
         </div>
       </article>
     </li>
