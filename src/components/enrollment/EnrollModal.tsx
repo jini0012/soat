bab39181c1555cf3e8afdf4b85c202502a933095
HwiGyoung Lee <ter0000@naver.com>
2025-02-16 15:29:41 +0900

    [add] : title과 selectedDate를 props로 받아 readOnly Input의 value로 보여주는 기능 추가

--- src/components/enrollment/EnrollModal.tsx
+++ src/components/enrollment/EnrollModal.tsx
@@ -2,13 +2,14 @@
 import React from "react";
 import { TextInput } from "../controls/Inputs";
 import { Button } from "../controls/Button";
+import { EnrollModalProps } from "@/types/enrollment";
 
-export default function EnrollModal() {
+export default function EnrollModal({ title, selectedDate }: EnrollModalProps) {
   return (
     <>
       <h3 className="sr-only">공연 등록하기</h3>
-      <TextInput label="공연명" value={""} readOnly />
-      <TextInput label="공연날짜" value={""} readOnly />
+      <TextInput label="공연명" value={title} readOnly />
+      <TextInput label="공연날짜" value={selectedDate} readOnly />
       <TextInput label="공연시간" value={""} />
       <Button className="ml-auto mt-4" type="button">
         추가
