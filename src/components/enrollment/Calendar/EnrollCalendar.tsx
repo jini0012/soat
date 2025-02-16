876fb1d448277eb714f50f73c9bedfe757f349b6
HwiGyoung Lee <ter0000@naver.com>
2025-02-16 15:27:05 +0900

    [add] : Calendar에서 선택된 Date를 ISOString의 형식으로 변환하여 부모 컴포넌트로 전달하는 기능 추가

--- src/components/enrollment/Calendar/EnrollCalendar.tsx
+++ src/components/enrollment/Calendar/EnrollCalendar.tsx
@@ -3,17 +3,39 @@ import Calendar from "react-calendar";
 import { Button } from "../../controls/Button";
 import { CalendarValue, EnrollCalendarProps } from "@/types/enrollment";
 import "./enrollCalendar.css";
+import { Value } from "react-calendar/dist/esm/shared/types.js";
 
-export default function EnrollCalendar({ openModal }: EnrollCalendarProps) {
-  const [value, onChange] = useState<CalendarValue>(new Date());
+export default function EnrollCalendar({
+  openModal,
+  setSelectedDate,
+}: EnrollCalendarProps) {
+  const [value, setValue] = useState<CalendarValue>(new Date());
 
+  const updateValue = (date: Date) => {
+    setValue(date);
+  };
+
+  const handleSelectedDate = (date: Date) => {
+    const correction = date.getTimezoneOffset() * 60000; // 분 단위를 밀리초로 변경
+    const formattedDate = new Date(date.getTime() - correction)
+      .toISOString()
+      .split("T")[0];
+    setSelectedDate(formattedDate);
+  };
+
+  const handleDateChange = (value: Value) => {
+    console.log(value);
+    if (!value || Array.isArray(value)) return; // 다중 선택 방지
+    updateValue(value);
+    handleSelectedDate(value);
+  };
   return (
     <>
       <Calendar
         locale="ko"
         formatDay={(locale, date) => `${date.getDate()}`}
         value={value}
-        onChange={onChange}
+        onChange={handleDateChange}
         tileClassName={({ date }) => (date.getDay() === 6 ? "saturday" : null)}
       />
       <Button className="ml-auto" type="button" onClick={openModal}>
