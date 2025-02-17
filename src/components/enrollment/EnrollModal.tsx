5c2be2dfde14b128817e390568aa2fdc346efd8d
HwiGyoung Lee <ter0000@naver.com>
2025-02-17 15:07:09 +0900

    [add] : 캐스팅 입력을 동적으로 받는 기능 추가

--- src/components/enrollment/EnrollModal.tsx
+++ src/components/enrollment/EnrollModal.tsx
@@ -1,23 +1,71 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { TextInput } from "../controls/Inputs";
 import { Button } from "../controls/Button";
 import { EnrollModalProps } from "@/types/enrollment";
 
-export default function EnrollModal({ title, selectedDate }: EnrollModalProps) {
+export default function EnrollModal({
+  title,
+  selectedDate,
+  onClose,
+}: EnrollModalProps) {
+  const [time, setTime] = useState<string>("");
+  const [casting, setCasting] = useState<string[]>([""]);
+
+  const addCasting = () => {
+    setCasting((prev) => [...prev, ""]);
+  };
+
+  const handleChangeCasting = (value: string, index: number) => {
+    setCasting((prev) => {
+      const newCasting = [...prev];
+      newCasting[index] = value;
+      return newCasting;
+    });
+  };
+
+  const delCasting = () => {
+    if (casting.length <= 1) {
+      return;
+    }
+    setCasting((prev) => prev.slice(0, -1));
+  };
+
   return (
     <>
       <h3 className="sr-only">공연 등록하기</h3>
       <TextInput label="공연명" value={title} readOnly />
       <TextInput label="공연날짜" value={selectedDate} readOnly />
-      <TextInput label="공연시간" value={""} />
-      <Button className="ml-auto mt-4" type="button">
-        추가
-      </Button>
-      <TextInput label="캐스팅" value={""} />
+      <TextInput label="공연시간" type="time" value={time} onChange={setTime} />
+      <div className="flex mt-4 gap-4 justify-end">
+        <Button
+          className=""
+          type="button"
+          onClick={delCasting}
+          disabled={casting.length <= 1}
+        >
+          제거
+        </Button>
+        <Button type="button" onClick={addCasting}>
+          추가
+        </Button>
+      </div>
+
+      <section className="h-[200px] overflow-y-auto">
+        {casting.map((cast, index) => (
+          <TextInput
+            label={`캐스팅${index + 1}`}
+            key={index}
+            value={cast}
+            onChange={(newCasting) => handleChangeCasting(newCasting, index)}
+          />
+        ))}
+      </section>
 
       <footer className="flex justify-between mt-4">
-        <Button type="button">취소</Button>
+        <Button type="button" onClick={onClose}>
+          취소
+        </Button>
         <Button type="button">등록</Button>
       </footer>
     </>
