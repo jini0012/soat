3a7123cfbef94cb2b766dd618fd69b9d5c5e8cb0
ddhsl <dnjs9199@naver.com>
2025-02-17 18:25:58 +0900

    [edit] SortFilter 컴포넌트 수정 : 검색 결과 정렬 내용 추가 및 styling

--- src/components/searchResult/SortFilter.tsx
+++ src/components/searchResult/SortFilter.tsx
@@ -1,12 +1,71 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+
 export default function SortFilter() {
+  const options = ["최근날짜순", "한줄평순", "낮은가격순"];
+  const [isOpenSortFilter, setIsOpenSortFilter] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const toggleSortFilter = () => {
+    setIsOpenSortFilter((prev) => !prev);
+  };
+
+  const handleOptionSelect = (option: string) => {
+    setSelectedOption(option);
+    setIsOpenSortFilter(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setIsOpenSortFilter(false);
+      }
+    };
+
+    if (isOpenSortFilter) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpenSortFilter]);
+
   return (
-    <>
-      <button className="text-xs">최근날짜순</button>
-      <div className="absolute top-[200px] rounded-md bg-white right-6 text-sm p-3 w-[88px] h-[105px] border border-gray-300 flex flex-col gap-2 shadow-lg ">
-        <div>최근날짜순</div>
-        <div>한줄평순</div>
-        <div>낮은가격순</div>
-      </div>
-    </>
+    <div className="relative inline-block">
+      <button
+        ref={buttonRef}
+        onClick={toggleSortFilter}
+        className="text-xs pr-2 bg-filter-icon w-[76px] h-[32px] bg-no-repeat bg-right bg-[length:15px]"
+      >
+        {selectedOption}
+      </button>
+
+      {isOpenSortFilter && (
+        <div
+          ref={dropdownRef}
+          className="absolute right-[0.5px]  w-[93px] rounded-md bg-white text-[13px]  p-3 border border-gray-300 flex flex-col gap-2 shadow-lg"
+        >
+          {options.map((option) => (
+            <div
+              key={option}
+              onClick={() => handleOptionSelect(option)}
+              className={`cursor-pointer rounded ${
+                selectedOption === option ? "text-flesh-500 font-bold" : ""
+              }`}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
