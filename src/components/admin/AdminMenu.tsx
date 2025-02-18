2ffc1cbdfdec63e8631d439c67548605f4defea4
ddhsl <dnjs9199@naver.com>
2025-02-18 18:29:19 +0900

    [edit] AdminMenu 컴포넌트 수정 : 쏘앳 바로가기 아이콘 추가

--- src/components/admin/AdminMenu.tsx
+++ src/components/admin/AdminMenu.tsx
@@ -64,8 +64,24 @@ export default function AdminMenu({ onClose }: AdminMenuProps) {
             />
           </button>
         </div>
-        <div className="border-b-[1.5px] border-b-flesh-500 h-[35px] flex items-center">
-          <Link href="" className="no-underline text-flesh-500  px-4">
+        <div className="border-b-[1.5px] border-b-flesh-500 h-[35px] flex items-center px-[6%]">
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6 2.66667H2.66667C2.22464 2.66667 1.80072 2.84226 1.48816 3.15482C1.17559 3.46738 1 3.89131 1 4.33333V12.6667C1 13.1087 1.17559 13.5326 1.48816 13.8452C1.80072 14.1577 2.22464 14.3333 2.66667 14.3333H11C11.442 14.3333 11.866 14.1577 12.1785 13.8452C12.4911 13.5326 12.6667 13.1087 12.6667 12.6667V9.33333M9.33333 1H14.3333M14.3333 1V6M14.3333 1L6 9.33333"
+              stroke="#FC4C13"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+
+          <Link href="" className="no-underline text-flesh-500  px-2">
             So@ 바로가기
           </Link>
         </div>
