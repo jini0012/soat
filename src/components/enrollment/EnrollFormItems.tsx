23ca91fc1e1208605c277367b0d30f508a93f527
HwiGyoung Lee <ter0000@naver.com>
2025-02-18 17:57:24 +0900

    [edit] : UI 컴포넌트와 상태 컴포넌트로 변경

--- src/components/enrollment/EnrollFormItems.tsx
+++ src/components/enrollment/EnrollFormItems.tsx
@@ -1,44 +1,33 @@
 "use client";
-import { TextInput } from "@/components/controls/Inputs";
 import React from "react";
-import { EnrollFormItemsProps } from "../../types/enrollment";
+import EnrollFormItemsUI from "./EnrollFormItemsUI";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { useDispatch } from "react-redux";
+import { updateStringFormField } from "@/redux/slices/enrollSlice";
+import { EnrollFormData } from "@/types/enrollment";
 
-export default function EnrollFormItems({
-  type,
-  title,
-  category,
-  bookingStartDate,
-  location,
-  onChange,
-}: EnrollFormItemsProps) {
+export default function EnrollFormItems() {
+  const { title, type, category, bookingStartDate, location } = useSelector(
+    (state: RootState) => state.enroll
+  );
+  const dispatch = useDispatch();
+
+  const handleOnChangeInputs = (
+    field: keyof Omit<EnrollFormData, "performances" | "poster">,
+    value: string
+  ) => {
+    dispatch(updateStringFormField({ field, value }));
+  };
+
   return (
-    <>
-      <TextInput
-        label="공연유형"
-        value={type}
-        onChange={(value) => onChange("type", value)}
-      />
-      <TextInput
-        label="공연명"
-        value={title}
-        onChange={(value) => onChange("title", value)}
-      />
-      <TextInput
-        label="카테고리"
-        value={category}
-        onChange={(value) => onChange("category", value)}
-      />
-      <TextInput
-        label="예매시작일"
-        type="date"
-        value={bookingStartDate}
-        onChange={(value) => onChange("bookingStartDate", value)}
-      />
-      <TextInput
-        label="위치"
-        value={location}
-        onChange={(value) => onChange("location", value)}
-      />
-    </>
+    <EnrollFormItemsUI
+      title={title}
+      type={type}
+      category={category}
+      bookingStartDate={bookingStartDate}
+      location={location}
+      onChange={handleOnChangeInputs}
+    />
   );
 }
