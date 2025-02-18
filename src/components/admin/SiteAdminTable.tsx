754b8e4985cfeec8acf05891763dbfcf1b18cd2a
ddhsl <dnjs9199@naver.com>
2025-02-18 17:15:07 +0900

    [edit] SiteAdminTable 컴포넌트 수정 : 사이트 관리자 계정 목록 클릭 시 모달 open 구현

--- src/components/admin/SiteAdminTable.tsx
+++ src/components/admin/SiteAdminTable.tsx
@@ -1,13 +1,19 @@
+"use client";
+import React, { useState } from "react";
 import { SiteAdmin } from "@/types/admin";
 import TableRow from "./TableRow";
 import TableHeader from "./TableHeader";
+import SiteAdminModify from "./SiteAdminModify"; // SiteAdminModify import
 
 export default function SiteAdminTable({
   data,
 }: {
-  data: SiteAdmin[]; //data의 타입 선언 - 배열로 반환
+  data: SiteAdmin[]; // data prop 정의
 }) {
-  const headers = ["이메일", "관리자명", "권한"]; //테이블 헤더명 선언
+  const [selectedUser, setSelectedUser] = useState<SiteAdmin | null>(null); // 선택된 관리자 상태
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 열기/닫기 상태
+
+  const headers = ["이메일", "관리자명", "권한"]; // 테이블 헤더명 선언
 
   const fieldMapping: {
     [key in (typeof headers)[number]]: keyof SiteAdmin;
@@ -17,27 +23,44 @@ export default function SiteAdminTable({
     권한: "permissions",
   };
 
+  const handleRowClick = (user: SiteAdmin) => {
+    setSelectedUser(user);
+    setIsModalOpen(true); // 행 클릭 시 모달 열기
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    setSelectedUser(null); // 모달 닫기 시 선택된 사용자 초기화
+  };
+
   return (
-    <table className="w-full table-fixed border-collapse">
-      <TableHeader headers={headers} />
-      <tbody>
-        {data.length > 0 ? (
-          data.map((newTheaterAdmin, index) => (
-            <TableRow
-              key={index}
-              rowData={newTheaterAdmin}
-              headers={headers}
-              fieldMapping={fieldMapping} // 필드 매핑 전달
-            />
-          ))
-        ) : (
-          <tr>
-            <td colSpan={headers.length} className="text-center py-1">
-              데이터가 없습니다.
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+    <>
+      <table className="w-full table-fixed border-collapse">
+        <TableHeader headers={headers} />
+        <tbody>
+          {data.length > 0 ? (
+            data.map((newTheaterAdmin, index) => (
+              <TableRow
+                key={index}
+                rowData={newTheaterAdmin}
+                headers={headers}
+                fieldMapping={fieldMapping}
+                onClick={() => handleRowClick(newTheaterAdmin)} // 행 클릭 시 모달 열기
+              />
+            ))
+          ) : (
+            <tr>
+              <td colSpan={headers.length} className="text-center py-1">
+                데이터가 없습니다.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+      {isModalOpen && selectedUser && (
+        <SiteAdminModify user={selectedUser} onClose={handleModalClose} />
+      )}
+    </>
   );
 }
