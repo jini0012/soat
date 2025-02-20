265c4d35894499183747fd827e6c1a8e49f8afe8
jini0012 <jinhui.ha1208@gmail.com>
2025-02-20 16:13:57 +0900

    [edit] account 페이지 내 UserInfo 및 MyReservation 컴포넌트 반응형 css 적용

--- src/components/account/ReservationItem.tsx
+++ src/components/account/ReservationItem.tsx
@@ -8,7 +8,7 @@ interface ReservationDataProps {
 }
 
 function Ul({ children }: { children: React.ReactNode }) {
-  return <ul className="flex justify-between">{children}</ul>;
+  return <ul className="flex justify-between gap-[10px]">{children}</ul>;
 }
 
 function Li({
@@ -19,17 +19,24 @@ function Li({
   isViewComplete,
 }: ReservationDataProps) {
   return (
-    <li className="w-[90px] h-[162px]">
+    <li className="size-full aspect-[90/162] w-[25vw] sm:w-[18vw] lg:w-[20vw] xl:w-[23vw]">
       <Link href={`/myPage/myBook/${bookId}`}>
         <article className="relative">
           <img
             src={`/${imageUrl}`}
             alt="공연 info"
+            className={`bg-flesh-500 rounded-[10px] mb-1 aspect-[90/130] object-cover ${
+              isViewComplete && "blur-[1px]"
+            }`}
           />
-          <h3 className="text-[10px] font-bold text-black truncate">{title}</h3>
-          <p className="text-[8px] text-gray-300">{viewDate}</p>
+          <h3 className="text-[10px] font-bold text-black w-full sm:text-xs md:text-sm lg:text-lg xl:text-2xl truncate">
+            {title}
+          </h3>
+          <p className="text-[8px] text-gray-300 sm:text-xs md:text-sm lg:text-lg xl:text-2xl">
+            {viewDate}
+          </p>
           {isViewComplete && (
-            <span className="absolute top-[50px] left-[25px] text-white text-xs">
+            <span className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transform rotate-[-50deg] text-white text-xs sm:text-sm md:text-2xl lg:text-4xl">
               관람완료
             </span>
           )}
