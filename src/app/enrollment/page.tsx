2b8c21c91bda043ee36b7bc200b756705fcabdc7
HwiGyoung Lee <ter0000@naver.com>
2025-02-20 17:15:22 +0900

    [edit] : redux로 상태를 관리하여 ssr 컴포넌트로 변경

--- src/app/enrollment/page.tsx
+++ src/app/enrollment/page.tsx
@@ -1,94 +1,35 @@
-"use client";
-import React, { useState } from "react";
-import { EnrollFormData } from "../../types/enrollment";
+import React from "react";
+
 import EnrollFormItems from "../../components/enrollment/EnrollFormItems";
 import EnrollPoster from "../../components/enrollment/EnrollPoster";
 import EnrollCalendar from "../../components/enrollment/Calendar/EnrollCalendar";
 import { Button } from "@/components/controls/Button";
-import EnrollModal from "@/components/enrollment/EnrollModal";
-import Modal from "@/components/Modal";
 import dynamic from "next/dynamic";
+
 const Editor = dynamic(() => import("@/components/editor/Editor"), {
   ssr: false,
 });
 
 export default function EnrollmentPage() {
-  const [formData, setFormData] = useState<EnrollFormData>({
-    type: "",
-    title: "",
-    category: "",
-    bookingStartDate: "",
-    location: "",
-    performances: [],
-    poster: null,
-  });
-  const [selectedDate, setSelectedDate] = useState<string>("");
-  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
-
-  const handleOpenModal = () => {
-    setIsOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsOpenModal(false);
-  };
-
-  const handleOnPoster = (file: File | null) => {
-    setFormData((prev) => ({
-      ...prev,
-      poster: file,
-    }));
-  };
-
-  const handleOnPerformance = (time: string, casting: string[]) => {
-    setFormData((prev) => {
-      const prevPerformances = [...prev.performances];
-      const newPerformance = {
-        date: selectedDate,
-        time: time,
-        casting: casting,
-      };
-
-      return {
-        ...prev,
-        performanceTime: [...prevPerformances, newPerformance],
-      };
-    });
-  };
-
-  const handleOnChangeInputs = (field: keyof EnrollFormData, value: string) => {
-    setFormData((prev) => ({
-      ...prev,
-      [field]: value,
-    }));
-  };
-
   return (
     <section className="max-w-[1920px] m-auto mb-[140px] px-[80px]">
       <h2 className="sr-only">공연 정보 등록페이지</h2>
       <form>
         <div className="flex flex-row gap-16">
-          <section className="w-[21.6%]">
+          <section className="w-[21.6%] h-full">
             <h3 className="sr-only">공연 포스터</h3>
-            <EnrollPoster onPosterChange={handleOnPoster} />
+            <EnrollPoster />
           </section>
-          <section className="w-[45.3%]">
+          <section className="w-[45.3%] h-full">
             <h3 className="sr-only">공연 정보</h3>
-            <EnrollFormItems
-              title={formData.title}
-              type={formData.type}
-              category={formData.category}
-              bookingStartDate={formData.bookingStartDate}
-              location={formData.location}
-              onChange={handleOnChangeInputs}
-            />
+            <EnrollFormItems />
           </section>
-          <section className="w-[28.125%] border rounded-[10px] flex flex-col p-4">
+          <section
+            id="calendar-container"
+            className="w-[28.125%] h-full border rounded-[10px] flex flex-col p-4"
+          >
             <h3 className="sr-only">공연 날짜</h3>
-            <EnrollCalendar
-              openModal={handleOpenModal}
-              setSelectedDate={setSelectedDate}
-            />
+            <EnrollCalendar />
           </section>
         </div>
         <section className="w-full mt-16 min-h-[600px]">
@@ -96,14 +37,6 @@ export default function EnrollmentPage() {
           <Editor />
         </section>
       </form>
-      <Modal isOpen={isOpenModal} onClose={handleCloseModal}>
-        <EnrollModal
-          onConfirm={handleOnPerformance}
-          onClose={handleCloseModal}
-          title={formData.title}
-          selectedDate={selectedDate}
-        />
-      </Modal>
       <footer className="fixed left-0 bottom-0 bg-flesh-200 w-full h-[120px] flex justify-end items-center pr-[60px] gap-14">
         <Button type="button">임시 저장</Button>
         <Button type="submit">공연 등록</Button>
