90135306ab029b3eef08162f784124f0b57db546
김지훈 <kim@jihun.io>
2025-02-21 15:40:52 +0900

    [add] 좌석 생성기 데모 페이지에 선택기로 이동 버튼 추가

--- src/app/demo/seats/generator/page.tsx
+++ src/app/demo/seats/generator/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/controls/Button";
 import TheaterSeatManager from "@/components/seats/TheaterLayoutManager";
 
 export default function SeatsGeneratorDemo() {
-  return <TheaterSeatManager />;
+  return (
+    <>
+      <TheaterSeatManager />
+      <Button href="/demo/seats/selector">좌석 선택 데모로 이동</Button>
+    </>
+  );
 }
