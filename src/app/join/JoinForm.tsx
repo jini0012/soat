98652e41f83874f0f86c6ed29d46e2698d7a369e
jini0012 <jinhui.ha1208@gmail.com>
2025-02-22 18:52:27 +0900

    [edit] JoinForm 컴포넌트 수정 및 유효성 검사 로직 util 적용

--- src/app/join/JoinForm.tsx
+++ src/app/join/JoinForm.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/controls/Button";
+import { JoinInput } from "@/components/controls/Inputs";
 import { Checkbox } from "@/components/controls/Inputs";
-import {
-  EmailInput,
-  PasswordInput,
-  NameAndTeamInput,
-  PhoneInput,
-  BusinessNumInput,
-} from "@/components/validation/FormValidation";
 import Link from "next/link";
+import { validations } from "@/utils/validations";
 
 interface JoinFormProps {
   setUserType: (userType: "buyer" | "seller") => void;
@@ -25,6 +20,33 @@ export default function JoinForm({
   const [isBusiness, setIsBusiness] = useState<boolean>(false);
   const [checkAge, setCheckAge] = useState(false);
   const [checkAgree, setCheckAgree] = useState(false);
+  const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
+  const [isSendEmail, setIsSendEmail] = useState<boolean>(false);
+  const [verifyNum, setVerifyNum] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [userName, setUserName] = useState("");
+  const [teamName, setTeamName] = useState("");
+  const [managerName, setManagerName] = useState("");
+  const [userPhone, setUserPhone] = useState("");
+  const [businessNum, setBusinessNum] = useState("");
+  const [isBusinessNumValid, setIsBusinessNumValid] = useState<boolean>(false);
+  const isEmailInputValid = validations.email.safeParse(email).success;
+  const isBusinessNumInputValid =
+    validations.businessNum.safeParse(businessNum).success;
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isSendEmail && !isEmailValid) {
+      timer = setTimeout(() => {
+        setIsSendEmail(false);
+      }, 60000);
+    }
+    return () => {
+      if (timer) clearTimeout(timer);
+    };
+  }, [isSendEmail, isEmailValid]);
 
   return (
     <>
@@ -54,10 +76,100 @@ export default function JoinForm({
         </li>
       </ul>
       <form className="w-full sm:max-w-[525px] flex flex-col border-2 rounded-lg border-flesh-200 px-5 py-4 gap-[10px] relative sm:gap-[20px]">
-        <EmailInput />
-        <PasswordInput />
-        <NameAndTeamInput userType={userType} />
-        <PhoneInput />
+        <JoinInput
+          label="이메일"
+          type="email"
+          value={email}
+          onChange={setEmail}
+          validation={validations.email}
+          disabled={isSendEmail}
+          message={
+            isSendEmail
+              ? "인증 번호가 이메일로 발송되었습니다. 이메일을 확인해 주세요"
+              : ""
+          }
+        >
+          <Button
+            highlight={true}
+            type="button"
+            onClick={(e) => {
+              setIsSendEmail(true);
+              (e.target as HTMLButtonElement).textContent = "재전송";
+            }}
+            size="small"
+            className="mb-1 py-[2.5px] sm:max-w-32 sm:py-none sm:text-base sm:font-bold"
+            disabled={!isEmailInputValid || isSendEmail}
+          >
+            인증번호 받기
+          </Button>
+        </JoinInput>
+        <JoinInput
+          label="인증번호"
+          value={verifyNum}
+          onChange={setVerifyNum}
+          disabled={!isSendEmail || isEmailValid}
+          validation={validations.emailVerifyNum}
+          message={isEmailValid ? "이메일 인증이 완료되었습니다." : ""}
+        >
+          <Button
+            highlight={true}
+            type="button"
+            onClick={() => {
+              setIsEmailValid(true);
+            }}
+            size="small"
+            className="mb-1 py-[2.5px] sm:max-w-16 sm:py-none sm:text-base sm:font-bold"
+            disabled={!isSendEmail || isEmailValid}
+          >
+            확인
+          </Button>
+        </JoinInput>
+        <JoinInput
+          label="비밀번호"
+          value={password}
+          onChange={setPassword}
+          placeholder="8~24자의 영문, 숫자, 특수문자"
+          validation={validations.password}
+        />
+        <JoinInput
+          label="비밀번호 확인"
+          value={passwordConfirm}
+          onChange={setPasswordConfirm}
+          validation={validations.passwordConfirm(passwordConfirm)}
+        />
+        {userType === "buyer" ? (
+          <JoinInput
+            label="이름"
+            value={userName}
+            onChange={setUserName}
+            placeholder="10자 이내의 국문 또는 영문"
+            validation={validations.name}
+          />
+        ) : (
+          <>
+            <JoinInput
+              label="팀명"
+              value={teamName}
+              onChange={setTeamName}
+              placeholder="10자 이내의 국문 또는 영문"
+              validation={validations.teamName}
+            />
+            <JoinInput
+              label="관리자명"
+              value={managerName}
+              onChange={setManagerName}
+              placeholder="10자 이내의 국문 또는 영문"
+              validation={validations.managerName}
+            />
+          </>
+        )}
+        <JoinInput
+          label="휴대폰"
+          value={userPhone}
+          onChange={setUserPhone}
+          placeholder="11자 숫자 (‘-’ 문자 제외)"
+          validation={validations.phone}
+        />
         {userType === "seller" && (
           <>
             <ul className="flex justify-center mb-2 gap-2">
@@ -84,7 +196,34 @@ export default function JoinForm({
                 </Button>
               </li>
             </ul>
-            {isBusiness && <BusinessNumInput isBusiness={isBusiness} />}
+            {isBusiness && (
+              <>
+                <JoinInput
+                  label="사업자등록번호"
+                  value={businessNum}
+                  onChange={setBusinessNum}
+                  placeholder="10자 숫자 (‘-’ 문자 제외)"
+                  validation={validations.businessNum}
+                  message={
+                    isBusinessNumValid
+                      ? "사업자 번호 인증이 완료되었습니다."
+                      : ""
+                  }
+                />
+                <Button
+                  highlight={true}
+                  size="small"
+                  className="mb-2 py-[2.5px] sm:text-base sm:font-bold"
+                  disabled={!isBusinessNumInputValid || isBusinessNumValid}
+                  onClick={() => {
+                    setIsBusinessNumValid(true);
+                  }}
+                  type="button"
+                >
+                  사업자등록번호 인증
+                </Button>
+              </>
+            )}
           </>
         )}
         {userType === "buyer" && (
