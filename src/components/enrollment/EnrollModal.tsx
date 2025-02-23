3ff626c53675a024971c5e906a6bb1c9bec09f2a
HwiGyoung Lee <ter0000@naver.com>
2025-02-23 15:03:46 +0900

    [add] : 범위 선택시 날짜 표시 기능 추가

--- src/components/enrollment/EnrollModal.tsx
+++ src/components/enrollment/EnrollModal.tsx
@@ -5,9 +5,11 @@ import { Button } from "../controls/Button";
 import { EnrollModalProps } from "@/types/enrollment";
 import { RootState } from "@/redux/store";
 import { useSelector } from "react-redux";
+import { format } from "date-fns";
+import { Value } from "react-calendar/dist/esm/shared/types.js";
 
 export default function EnrollModal({
-  selectedDate,
+  selectedDates,
   onClose,
   onConfirm,
 }: EnrollModalProps) {
@@ -43,20 +45,60 @@ export default function EnrollModal({
     onClose();
   };
 
-  return (
-    <>
-      <h3 className="sr-only">공연 등록하기</h3>
-      <TextInput label="공연명" value={title} readOnly />
-      <span>공연날짜</span>
-      <div className="flex px-4 py-2 flex-1 w-full focus-visible:outline-none bg-background border-2 rounded-lg overflow-x-scroll">
-        {selectedDate.map((date, idx) => (
+  const generateDateRange = (startDate: Value, endDate: Value) => {
+    if (!startDate && !endDate) {
+      return;
+    }
+    const start = new Date(startDate as Date);
+    const end = new Date(endDate as Date);
+    const dates = [];
+
+    while (start <= end) {
+      dates.push(format(start, "yyyy-MM-dd"));
+      start.setDate(start.getDate() + 1);
+    }
+    return dates;
+  };
+
+  const renderDates = () => {
+    if (!selectedDates) {
+      return;
+    }
+
+    if (Array.isArray(selectedDates)) {
+      console.log("배열", selectedDates);
+      const generatedDates = generateDateRange(
+        selectedDates[0],
+        selectedDates[1]
+      );
+
+      return generatedDates?.map((date, idx) => {
+        return (
           <p
             className="bg-flesh-600 py-1 px-2 mr-2 text-background rounded-lg text-md whitespace-nowrap"
             key={idx}
           >
             {date}
           </p>
-        ))}
+        );
+      });
+    }
+
+    const date = format(new Date(selectedDates), "yyyy-MM-dd");
+    return (
+      <p className="bg-flesh-600 py-1 px-2 mr-2 text-background rounded-lg text-md whitespace-nowrap">
+        {date}
+      </p>
+    );
+  };
+
+  return (
+    <>
+      <h3 className="sr-only">공연 등록하기</h3>
+      <TextInput label="공연명" value={title} readOnly />
+      <span>공연날짜</span>
+      <div className="flex px-4 py-2 flex-1 w-full focus-visible:outline-none bg-background border-2 rounded-lg overflow-x-scroll">
+        {renderDates()}
       </div>
       <TextInput label="공연시간" type="time" value={time} onChange={setTime} />
       <div className="flex mt-4 gap-4 justify-end ">
