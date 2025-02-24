e78291f86337749efc4aea3db25bcc02b4be0ff9
HwiGyoung Lee <ter0000@naver.com>
2025-02-24 14:44:32 +0900

    [add] : KakaoAddressData Type 추가

--- src/types/kakao.ts
+++ src/types/kakao.ts
@@ -1,11 +1,13 @@
-interface KakaoAddressProps {
-  onComplete: (data: {
-    address: string;
-    zonecode: string;
-    roadAddress: string;
-    jibunAddress: string;
-    buildingName: string;
-  }) => void;
+export type KakaoAddressData = {
+  address: string;
+  zonecode: string;
+  roadAddress: string;
+  jibunAddress: string;
+  buildingName: string;
+};
+
+export interface KakaoAddressProps {
+  onComplete: (data: KakaoAddressData) => void;
   onClose?: () => void;
   buttonText?: string;
   className?: string;
