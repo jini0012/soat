88c44f21e049ad62219b150574c54c3b81b6aefa
HwiGyoung Lee <ter0000@naver.com>
2025-02-25 16:26:16 +0900

    [add] : Image파일을 추가할 수 있는 기능 추가

--- src/components/editor/Toolbar.tsx
+++ src/components/editor/Toolbar.tsx
@@ -1,6 +1,8 @@
+import { addFile } from "@/redux/slices/enrollSlice";
 import { EditorToolbarProps, Level } from "@/types/editor";
 import { ChainedCommands } from "@tiptap/react";
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
 
 export default function Toolbar({
   editor,
@@ -8,7 +10,10 @@ export default function Toolbar({
   formattingButtons,
   codeBlockButton,
   onClickCodeBlockButton,
+  imageInput,
 }: EditorToolbarProps) {
+  const dispatch = useDispatch();
+  const fileKey = useRef(1);
   const handleEditorHeadingOnClick = (level: Level) => {
     if (!editor) {
       return;
@@ -30,8 +35,54 @@ export default function Toolbar({
     editor.chain().focus().toggleHeading({ level: 1 });
   };
 
+  const imageToBase64 = (file: File): Promise<string> => {
+    return new Promise((res) => {
+      const blob = new Blob([file]);
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = () => {
+        const base64String = reader.result;
+        res(base64String as string);
+      };
+    });
+  };
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files) {
+      const prevUrls = await Promise.all(
+        Array.from(files).map(async (file, index) => {
+          const currentKey = fileKey.current + index;
+          const base64 = await imageToBase64(file); // base64 처리
+          dispatch(
+            addFile({
+              fileKey: currentKey,
+              fileName: file.name,
+              fileSize: file.size,
+              fileType: file.type,
+              base64Data: base64, // base64 데이터 Redux에 저장
+            })
+          );
+          return { src: base64, key: currentKey };
+        })
+      );
+
+      if (prevUrls && editor) {
+        prevUrls.forEach((item) => {
+          editor
+            .chain()
+            .focus()
+            .setImage({ src: item.src, key: item.key })
+            .run();
+        });
+      }
+      fileKey.current += files.length;
+    }
+  };
+
   return (
-    <div>
+    <div className="flex flex-wrap">
       {headingButtons.map(({ type, label, icon: Icon, level }, index) => {
         const isActive = editor?.isActive(type, { level });
         return (
@@ -75,6 +126,15 @@ export default function Toolbar({
       >
         {<codeBlockButton.icon />}
       </button>
+      <label className="p-2 m-1 border rounded">
+        {<imageInput.icon />}
+        <input
+          key={fileKey.current}
+          type="file"
+          className="sr-only"
+          onChange={handleFileChange}
+        />
+      </label>
     </div>
   );
 }
