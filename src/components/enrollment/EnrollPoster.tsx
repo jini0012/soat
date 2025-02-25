69a036c4498d42297f3d4cbd09ea0dcc921b9c9d
HwiGyoung Lee <ter0000@naver.com>
2025-02-25 16:40:44 +0900

    [edit] : poster의 미리보기를 base64 인코딩으로 변경 및 스타일링 변경

--- src/components/enrollment/EnrollPoster.tsx
+++ src/components/enrollment/EnrollPoster.tsx
@@ -9,24 +9,43 @@ export default function EnrollPoster() {
   const [previewPoster, setPreviewPoster] = useState<string | null>(null);
   const dispatch = useDispatch();
 
-  const handleFileOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const imageToBase64 = (file: File): Promise<string> => {
+    return new Promise((res) => {
+      const blob = new Blob([file]);
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = () => {
+        const base64String = reader.result;
+        res(base64String as string);
+      };
+    });
+  };
+
+  const handleFileOnChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
 
     if (file) {
-      const fileURL = URL.createObjectURL(file); // Blob URL 생성
-      setPreviewPoster(fileURL); // Blob URL을 미리보기로 설정
+      const base64File = await imageToBase64(file);
+      setPreviewPoster(base64File); // Blob URL을 미리보기로 설정
       setFileName(file.name);
-      dispatch(setPoster(file)); // 파일을 Redux에 저장
+      dispatch(
+        setPoster({
+          fileName: file.name,
+          fileSize: file.size,
+          fileType: file.type,
+          base64Data: base64File,
+        })
+      ); // 파일을 Redux에 저장
     }
   };
 
   return (
     <>
       <div className="relative lg:h-full w-full flex">
-        <div className="w-full h-full bg-gray-300 hidden lg:block">
+        <div className="w-full h-full border-2 hidden lg:block">
           {previewPoster && (
             <img
-              className="w-full h-full object-cover"
+              className="w-[380px] h-[520px] object-cover aspec-[270/210]"
               src={previewPoster}
               alt="포스터 미리보기"
             />
@@ -37,10 +56,10 @@ export default function EnrollPoster() {
           {fileName ? fileName : "파일을 선택하세요"}
         </div>
         <label
-          className="flex justify-center items-center  rounded-md lg:w-[20%] lg:aspect-[1/1] lg:rounded-full lg:absolute lg:bottom-0 lg:right-0 lg:bg-flesh-400 lg:z-100  cursor-pointer"
+          className="flex justify-center items-center  rounded-md lg:w-[36px] lg:aspect-[1/1] lg:rounded-full lg:absolute lg:bottom-5 lg:right-5 lg:bg-flesh-500 lg:z-100  cursor-pointer lg:hover:bg-flesh-700 transition"
           htmlFor="poster"
         >
-          <Plus className="" />
+          <Plus className="lg:stroke-white" strokeWidth={3} />
         </label>
 
         <input
