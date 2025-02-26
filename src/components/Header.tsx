b5b8c4348ff5809d6e2df9577aef67f348535369
jini0012 <jinhui.ha1208@gmail.com>
2025-02-27 02:08:22 +0900

    [edit] Header 컴포넌트 통합 및 반응형 스타일 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,18 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="m-auto w-full flex justify-center border-b-2 border-gray-300 box-border h-full max-h-[76px] sm:max-h-[132px]">
-      <h1 className="font-bold text-3xl italic py-5">
-        <Link href={"/"} className="text-flesh-600">
-          SO@
-        </Link>
-      </h1>
-    </header>
+    <>
+      <header className="m-auto w-full flex justify-center sm:mt-[50px] border-b-2 border-gray-300 box-border h-full max-h-[76px] sm:max-h-[132px] sm:border-none">
+        <h1 className="font-bold text-5xl italic py-5">
+          <Link href={"/"} className="text-flesh-600">
+            <img
+              src="images/icons/logo-temp.svg"
+              alt="soat"
+              className="sm:w-[123px]"
+            />
+          </Link>
+        </h1>
+      </header>
+    </>
   );
 }
