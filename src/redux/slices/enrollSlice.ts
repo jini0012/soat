5b09df411c995e47e7604f84ea592a9c18254f03
HwiGyoung Lee <ter0000@naver.com>
2025-02-26 19:02:13 +0900

    [add] : 수정 상태를 관리하는 isDirty State추가

--- src/redux/slices/enrollSlice.ts
+++ src/redux/slices/enrollSlice.ts
@@ -20,6 +20,7 @@ export interface EnrollState {
   performances: DailyPerformances;
   content: JSONContent;
   files: ImageFile[];
+  isDirty: boolean; //수정 상태를 관리하는 상태
 }
 
 const initialState: EnrollState = {
@@ -34,6 +35,7 @@ const initialState: EnrollState = {
   performances: {},
   content: {},
   files: [],
+  isDirty: false,
 };
 
 const enrollSlice = createSlice({
@@ -45,33 +47,42 @@ const enrollSlice = createSlice({
     },
     setTitle: (state, action: PayloadAction<string>) => {
       state.title = action.payload;
+      state.isDirty = true;
     },
     setCategory: (state, action: PayloadAction<string>) => {
       state.category = action.payload;
+      state.isDirty = true;
     },
     setBookingStartDate: (state, action: PayloadAction<string>) => {
       state.bookingStartDate = action.payload;
+      state.isDirty = true;
     },
     setAddress: (state, action: PayloadAction<string>) => {
       state.address = action.payload;
+      state.isDirty = true;
     },
     setDetailAddress: (state, action: PayloadAction<string>) => {
       state.detailAddress = action.payload;
+      state.isDirty = true;
     },
     setPostCode: (state, action: PayloadAction<string>) => {
       state.postCode = action.payload;
+      state.isDirty = true;
     },
     setPoster: (state, action: PayloadAction<ImageFile>) => {
       state.poster = action.payload;
+      state.isDirty = true;
     },
     setContent: (state, action: PayloadAction<JSONContent>) => {
       state.content = action.payload;
+      state.isDirty = true;
     },
     updateStringFormField: <T extends keyof EnrollFormFields>(
       state: EnrollState,
       action: PayloadAction<{ field: T; value: string }>
     ) => {
       state[action.payload.field] = action.payload.value;
+      state.isDirty = true;
     },
 
     addPerformance: (
@@ -92,6 +103,7 @@ const enrollSlice = createSlice({
           casting,
         });
       });
+      state.isDirty = true;
     },
 
     editPerformance: (
@@ -108,6 +120,7 @@ const enrollSlice = createSlice({
       if (state.performances[formattedDate]) {
         state.performances[formattedDate][index] = performance;
       }
+      state.isDirty = true;
     },
 
     removePerformance: (
@@ -130,14 +143,20 @@ const enrollSlice = createSlice({
           delete state.performances[formattedDate];
         }
       }
+      state.isDirty = true;
     },
     addFile: (state, action: PayloadAction<ImageFile>) => {
       state.files.push(action.payload);
+      state.isDirty = true;
     },
     deleteFile: (state, action: PayloadAction<number>) => {
       state.files = state.files.filter(
         (file) => file.fileKey !== action.payload
       );
+      state.isDirty = true;
+    },
+    resetDirty: (state) => {
+      state.isDirty = false;
     },
   },
 });
@@ -158,5 +177,6 @@ export const {
   updateStringFormField,
   addFile,
   deleteFile,
+  resetDirty,
 } = enrollSlice.actions;
 export default enrollSlice.reducer;
