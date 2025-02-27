ec2bf6bebf75337959094375a2b5e6422cc517ee
김지훈 <kim@jihun.io>
2025-02-27 17:39:52 +0900

    [add] 이메일 인증 번호 발송 기능 추가

---
+++ src/app/api/auth/email-verification/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import { adminDb } from "../../firebaseAdmin";
+
+async function sendEmail({ email, code }: { email: string; code: number }) {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  try {
+    const info = await transporter.sendMail({
+      from: `"SOAT" <noreply@${process.env.EMAIL_DOMAIN}>`,
+      to: email,
+      subject: "[SO@] 이메일 인증",
+      text: `인증 번호: ${code}`,
+      html: `
+        <h1>이메일 인증</h1>
+        <p>SO@의 회원 가입을 위한 이메일 인증을 진행합니다.</p>
+        <p>아래의 인증 번호를 입력해주세요.</p>
+        <p>인증 번호: <strong>${code}</strong></p>
+      `,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const { email } = data;
+
+  const code = Math.floor(Math.random() * 1000000);
+
+  sendEmail({ email, code });
+
+  adminDb
+    .collection("emailVerification")
+    .doc(email)
+    .set({ code, createdAt: new Date(), status: "pending" });
+
+  return NextResponse.json({ success: true });
+}
