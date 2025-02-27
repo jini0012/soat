ad5cbdc8ae338a11049b742a9ef2de1168296a1e
김지훈 <kim@jihun.io>
2025-02-27 20:12:55 +0900

    Revert "[edit] 사용자 정보를 props로 받아와서 표시하도록 수정"
    This reverts commit 7a35670b7b95677e59e28533351de0dcbe756373.

--- src/app/account/UserInfo.tsx
+++ src/app/account/UserInfo.tsx
@@ -1,25 +1,15 @@
 import Li from "@/components/account/UserInfoItem";
 import Link from "next/link";
 
-export default function UserInfo({
-  username,
-  email,
-  phone,
-  userType,
-}: {
-  username: string;
-  email: string;
-  phone: string;
-  userType: string;
-}) {
+export default function UserInfo() {
   return (
     <section className="sm:row-span-2 sm:flex sm:flex-col sm:border-r-2 sm:pr-6">
       <h2 className="text-sm sm:text-3xl sm:my-6 sm:font-bold">회원 정보</h2>
       <ul className="relative p-5 rounded-[10px] border-2 border-flesh-200 text-xs sm:h-80 sm:flex sm:flex-col sm:justify-center sm:gap-2 whitespace-nowrap ">
-        <Li label="이름" data={username} />
-        <Li label="이메일" data={email} />
-        <Li label="휴대폰" data={phone} />
-        <Li label="회원 유형" data={userType} />
+        <Li label="이름" data="userName" />
+        <Li label="이메일" data="userEmail" />
+        <Li label="휴대폰" data="userPhone" />
+        <Li label="회원 유형" data="userType" />
         <li className="absolute bottom-[7px] right-[13px]">
           <Link
             href="/account/edit"
