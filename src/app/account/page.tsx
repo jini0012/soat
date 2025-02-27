39040b75465286f7eb5a0f4252eb839117ef504f
김지훈 <kim@jihun.io>
2025-02-27 20:12:50 +0900

    Revert "[add] 사용자 정보를 API로 조회하여 표시"
    This reverts commit fa3ea19842a9547a421cc9920e0aa1beaa53601a.

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,62 +1,16 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Header from "@/components/Header";
 import UserInfo from "./UserInfo";
 import MyReservation from "./MyReservation";
 
-import axios from "axios";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-
 export default function page() {
-  const { status } = useSession();
-  const router = useRouter();
-
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [userType, setUserType] = useState("");
-
-  useEffect(() => {
-    console.log(status);
-    if (status !== "authenticated" && status !== "loading") {
-      router.push("/login");
-    }
-
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get("/api/account/me");
-        const { user } = response.data;
-
-        setUsername(user.username);
-        setEmail(user.email);
-        setPhone(user.phoneNumber);
-        if (user.userType === "seller") {
-          setUserType("공연 관리자");
-        } else {
-          setUserType("예매 회원");
-        }
-      } catch (error) {
-        console.error("사용자 정보 조회 오류:", error);
-      }
-    };
-
-    fetchUser();
-  }, []);
-
   return (
     <>
       <Header />
       <main
         className={`grid relative m-auto w-full pt-[10px] px-[30px] sm:grid-flow-col sm:grid-rows-2 sm:gap-4 justify-center`}
       >
-        <UserInfo
-          username={username}
-          email={email}
-          phone={phone}
-          userType={userType}
-        />
+        <UserInfo />
         <MyReservation />
       </main>
     </>
