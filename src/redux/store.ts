fa2cbf93658486a982ea038f2d5f4274c9e894ef
HwiGyoung Lee <ter0000@naver.com>
2025-02-28 17:06:16 +0900

    [add] : redux-persist 충돌 해결을 위한 middleware 추가

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer, PersistConfig } from "redux-persist";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  PersistConfig,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import enrollReducer, { EnrollState } from "./slices/enrollSlice";
 import storage from "redux-persist/lib/storage";
 
 const enrollPersistConfig: PersistConfig<EnrollState> = {
   key: "enroll", // enroll 상태의 키
   storage, // localStorage 사용
-  throttle: 5000,
+  throttle: 10,
+  blacklist: ["isDirty"],
 };
 
 const persistedEnrollReducer = persistReducer(
@@ -16,13 +27,21 @@ const persistedEnrollReducer = persistReducer(
 
 //persistor.pause persistor.persist
 
-export const store = configureStore({
-  reducer: {
-    enroll: persistedEnrollReducer,
-  },
-  devTools: process.env.NODE_ENV !== "production",
+const rootReducer = combineReducers({
+  enroll: persistedEnrollReducer,
+  //reducer 추가 부분
 });
 
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== "production",
+});
 export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
