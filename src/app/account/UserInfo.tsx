7360e9261bc51b62ba97745e6e95bffc8f5f4087
jini0012 <jinhui.ha1208@gmail.com>
2025-02-28 13:07:43 +0900

    [edit] UserInfo 컴포넌트의 스타일에서 padding-right을 padding-x로 변경

--- src/app/account/UserInfo.tsx
+++ src/app/account/UserInfo.tsx
@@ -15,7 +15,7 @@ export default function UserInfo({
   userType,
 }: UserInfoProps) {
   return (
-    <section className="sm:row-span-2 sm:flex sm:flex-col sm:border-r-2 sm:pr-6">
+    <section className="sm:row-span-2 sm:flex sm:flex-col sm:border-r-2 sm:px-6">
       <h2 className="text-sm sm:text-3xl sm:my-6 sm:font-bold">회원 정보</h2>
       <div className="relative">
         <ul className="p-5 rounded-[10px] border-2 border-flesh-200 text-xs sm:h-80 sm:flex sm:flex-col sm:justify-center sm:gap-2 whitespace-nowrap ">
