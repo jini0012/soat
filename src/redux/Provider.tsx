4ee64b7fd874f6864d4e42d6863562d6fa913381
HwiGyoung Lee <ter0000@naver.com>
2025-03-01 22:22:07 +0900

    [edit] : PersistGator 제거

--- src/redux/Provider.tsx
+++ src/redux/Provider.tsx
@@ -1,18 +1,11 @@
 "use client";
 import { Provider } from "react-redux";
-import { persistor, store } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./store";
 
 export default function ReduxProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
