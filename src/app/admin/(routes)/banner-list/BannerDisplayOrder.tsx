7bbb5dcf0a2cdbc9093ac2200632ac566d177f1a
ddhsl <dnjs9199@naver.com>
2025-03-03 00:55:06 +0900

    [edit] BannerDisplayOrder 컴포넌트 수정 : 최소 / 최대 높이 값 지정

--- src/app/admin/(routes)/banner-list/BannerDisplayOrder.tsx
+++ src/app/admin/(routes)/banner-list/BannerDisplayOrder.tsx
@@ -7,7 +7,7 @@ export default function BannerDisplayOrder({ data }: { data: Banner[] }) {
     <>
       <ListTitle>배너 표시 순서</ListTitle>
       <p className="text-xs text-flesh-500">*활성화된 배너</p>
-      <div className="w-full h-[125px] bg-gray-100 border border-gray-300 mt-3 ">
+      <div className="w-full min-h-[150px] max-h-auto bg-gray-100 border border-gray-300 mt-3 ">
         <BannerDragAndDrop data={data} />
       </div>
     </>
