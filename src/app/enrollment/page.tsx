38b50ebc32c91e5a6b5b1bc766488cceb289caed
HwiGyoung Lee <ter0000@naver.com>
2025-03-03 18:02:31 +0900

    [edit] : 기존의 main 컨텐츠를 EnrollMains 컴포넌트로 분리

--- src/app/enrollment/page.tsx
+++ src/app/enrollment/page.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import Link from "next/link";
-import EnrollFormItems from "../../components/enrollment/EnrollFormItems";
-import EnrollPoster from "../../components/enrollment/EnrollPoster";
-import EnrollCalendar from "../../components/enrollment/Calendar/EnrollCalendar";
-import dynamic from "next/dynamic";
 import EnrollWrapper from "@/components/enrollment/EnrollWrapper";
-import EnrollFooter from "./EnrollFooter";
-const Editor = dynamic(() => import("@/components/editor/Editor"), {
-  ssr: false,
-});
+import EnrollFooter from "@/components/enrollment/EnrollFooter";
+import EnrollMains from "@/components/enrollment/EnrollMains";
 
 export default function EnrollmentPage() {
   return (
@@ -18,32 +12,7 @@ export default function EnrollmentPage() {
           <h1 className="font-bold text-3xl italic py-5">SO@</h1>
         </Link>
       </header>
-      <main className="max-w-[1920px] m-auto mb-[140px] px-[80px]">
-        <h2 className="sr-only">공연 정보 등록페이지</h2>
-        <form>
-          <div className="flex flex-row gap-4 flex-wrap">
-            <section className="lg:w-[45.3%] w-full h-full lg:order-1">
-              <h3 className="sr-only">공연 정보</h3>
-              <EnrollFormItems />
-            </section>
-            <section className="lg:w-[21.6%] w-full lg:order-0">
-              <h3 className="lg:sr-only">포스터</h3>
-              <EnrollPoster />
-            </section>
-            <section
-              id="calendar-container"
-              className="lg:w-[28.125%] h-full border rounded-[10px] flex flex-col p-4 lg:order-2"
-            >
-              <h3 className="sr-only">공연 날짜</h3>
-              <EnrollCalendar />
-            </section>
-          </div>
-          <section className="w-full mt-16 min-h-[600px]">
-            <h3 className="mb-4 text-base">공연 세부 정보</h3>
-            <Editor />
-          </section>
-        </form>
-      </main>
+      <EnrollMains />
       <EnrollWrapper>
         <EnrollFooter />
       </EnrollWrapper>
