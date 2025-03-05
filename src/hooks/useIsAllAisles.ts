807621731b63ff01711c59975bd0e333f36459ec
HwiGyoung Lee <ter0000@naver.com>
2025-03-05 19:25:18 +0900

    [add/edit] : 남은 row의 통로를 갱신하는 validateRemainingAisles 함수 추가 및 기존의 isAllAisle의 함수 네이밍을  isAisleInAllRows 로 변경

--- src/hooks/useIsAllAisles.ts
+++ src/hooks/useIsAllAisles.ts
@@ -1,12 +1,33 @@
 "use client";
 
+import { addIsAllAisle } from "@/redux/slices/seatSlice";
 import { RootState } from "@/redux/store";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
 export const useIsAllAisles = () => {
   const rowConfigs = useSelector((state: RootState) => state.seat.rowsConfigs);
+  const dispatch = useDispatch();
+  const validateRemainingAisles = () => {
+    const targetSeats = Object.values(rowConfigs).slice(0, -1);
 
-  const isAllAisle = (targetSeatLabel: string, targetAisles: number) => {
+    const aislesSet = new Set(
+      targetSeats
+        .map((rowConfig) => rowConfig.aisles)
+        .reduce((acc, aisles) => acc.concat(aisles), [])
+    );
+
+    aislesSet.forEach((aisle) => {
+      const isAllAisle = targetSeats
+        .filter((rowConfig) => rowConfig.seats >= aisle)
+        .every((rowConfig) => rowConfig.aisles.includes(aisle));
+      if (isAllAisle) {
+        dispatch(addIsAllAisle(aisle));
+      }
+    });
+  };
+
+  const isAisleInAllRows = (targetSeatLabel: string, targetAisles: number) => {
     return Object.keys(rowConfigs)
       .filter((seatLabel) => seatLabel !== targetSeatLabel)
       .filter((seatLabel) => rowConfigs[seatLabel].seats >= targetAisles)
@@ -16,5 +37,5 @@ export const useIsAllAisles = () => {
       });
   };
 
-  return isAllAisle;
+  return { validateRemainingAisles, isAisleInAllRows };
 };
