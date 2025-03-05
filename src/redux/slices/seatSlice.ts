fb31d62bbfb8bfc91d1803055365ec10377e318e
HwiGyoung Lee <ter0000@naver.com>
2025-03-05 18:29:55 +0900

    [add] :seat을 관리하는 reducer actions 추가

--- src/redux/slices/seatSlice.ts
+++ src/redux/slices/seatSlice.ts
@@ -1,11 +1,41 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RowConfigs } from "@/types/seat";
 
+const seatLabels = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+]; //고정된 값으로 재랜더링 필요 x
+
 interface SeatState {
   rows: number;
   rowsConfigs: RowConfigs;
   totalSeats: number;
   isDirty: boolean;
+  isAllAisle: number[];
 }
 const initialState: SeatState = {
   rows: 5,
@@ -34,6 +64,7 @@ const initialState: SeatState = {
     },
   },
   isDirty: false,
+  isAllAisle: [],
 };
 
 const seatSlice = createSlice({
@@ -46,9 +77,67 @@ const seatSlice = createSlice({
     setTotalSeats: (state, action: PayloadAction<number>) => {
       state.totalSeats = action.payload;
     },
+    addRowsConfigs: (state, action: PayloadAction<number>) => {
+      const key = seatLabels[action.payload];
+      state.rowsConfigs[key] = {
+        seats: 10,
+        aisles: [],
+      };
+    },
+    deleteRowsConfigs: (state, action: PayloadAction<number>) => {
+      const key = seatLabels[action.payload]; // seatLabels에서 인덱스를 기반으로 키를 가져옴
+      delete state.rowsConfigs[key];
+    },
+    updateSeats: (
+      state,
+      action: PayloadAction<{ rowLabel: string; newSeats: number }>
+    ) => {
+      const { rowLabel, newSeats } = action.payload;
+      if (state.rowsConfigs[rowLabel]) {
+        state.rowsConfigs[rowLabel].seats = newSeats;
+      }
+    },
+    removeAisles: (
+      state,
+      action: PayloadAction<{ rowLabel: string; aisleNumber: number }>
+    ) => {
+      const { rowLabel, aisleNumber } = action.payload;
+      if (state.rowsConfigs[rowLabel]) {
+        state.rowsConfigs[rowLabel].aisles = state.rowsConfigs[
+          rowLabel
+        ].aisles.filter((aisle) => aisle !== aisleNumber);
+      }
+    },
+    addAisles: (
+      state,
+      action: PayloadAction<{ rowLabel: string; aisleNumber: number }>
+    ) => {
+      const { rowLabel, aisleNumber } = action.payload;
+      if (state.rowsConfigs[rowLabel]) {
+        state.rowsConfigs[rowLabel].aisles.push(aisleNumber);
+      }
+    },
+    addIsAllAisle: (state, action: PayloadAction<number>) => {
+      state.isAllAisle.push(action.payload);
+    },
+    removeIsAllAisle: (state, action: PayloadAction<number>) => {
+      state.isAllAisle = state.isAllAisle.filter(
+        (aisle) => aisle !== action.payload
+      );
+    },
   },
 });
 
-export const { setRows, setTotalSeats } = seatSlice.actions;
+export const {
+  setRows,
+  setTotalSeats,
+  addRowsConfigs,
+  deleteRowsConfigs,
+  updateSeats,
+  removeAisles,
+  addAisles,
+  addIsAllAisle,
+  removeIsAllAisle,
+} = seatSlice.actions;
 
 export default seatSlice.reducer;
