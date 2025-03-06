5d09add3a43ce4f14f9d0123986b78f574509901
HwiGyoung Lee <ter0000@naver.com>
2025-03-06 18:42:29 +0900

    [edit] : onInput과 ref 추가

--- src/components/controls/TextArea.tsx
+++ src/components/controls/TextArea.tsx
@@ -1,9 +1,65 @@
 "use client";
-import React from "react";
+import React, { ForwardedRef, forwardRef } from "react";
 import { focusRings } from "@/styles/constants";
 import { TextAreaProps } from "@/types/controls";
 
-export default function TextArea({
+const TextArea = forwardRef(function Textarea(
+  props: TextAreaProps,
+  ref: ForwardedRef<HTMLTextAreaElement>
+) {
+  const {
+    label,
+    value,
+    onChange,
+    placeholder,
+    className = "",
+    disabled,
+    resize,
+    rows,
+    max,
+    onInput,
+  } = props;
+
+  const resizeClass = resize ? "resize" : "resize-none";
+
+  if (label) {
+    return (
+      <label className={`w-full ${className}`}>
+        {label}
+        <textarea
+          ref={ref}
+          className={`${resizeClass} border-2 rounded-lg px-4 py-2 w-full focus-visible:outline-none bg-background  ${focusRings.default} ${className}`}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+          disabled={disabled}
+          rows={rows}
+          maxLength={max}
+          onInput={onInput}
+        />
+      </label>
+    );
+  } else {
+    return (
+      <textarea
+        ref={ref}
+        className={`${resizeClass} border-2 rounded-lg px-4 py-2 w-full focus-visible:outline-none bg-background  ${focusRings.default} ${className}`}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        disabled={disabled}
+        rows={rows}
+        maxLength={max}
+        onInput={onInput}
+      />
+    );
+  }
+});
+
+TextArea.displayName = "TextArea";
+export default TextArea;
+
+/* export default function TextArea({
   label,
   value,
   onChange,
@@ -13,6 +69,7 @@ export default function TextArea({
   resize,
   rows,
   max,
+  onInput,
 }: TextAreaProps) {
   const resizeClass = resize ? "resize" : "resize-none";
 
@@ -21,27 +78,30 @@ export default function TextArea({
       <label className={`w-full ${className}`}>
         {label}
         <textarea
-          className={`${resizeClass} border-2 rounded-lg px-4 py-2 w-full focus-visible:outline-none bg-background ${className} ${focusRings.default}`}
+          className={`${resizeClass} border-2 rounded-lg px-4 py-2 w-full focus-visible:outline-none bg-background  ${focusRings.default} ${className}`}
           value={value}
           onChange={(e) => onChange(e.target.value)}
           placeholder={placeholder}
           disabled={disabled}
           rows={rows}
           maxLength={max}
+          onInput={onInput}
         />
       </label>
     );
   } else {
     return (
       <textarea
-        className={`${resizeClass} border-2 rounded-lg px-4 py-2 w-full focus-visible:outline-none bg-background ${className} ${focusRings.default}`}
+        className={`${resizeClass} border-2 rounded-lg px-4 py-2 w-full focus-visible:outline-none bg-background  ${focusRings.default} ${className}`}
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
         disabled={disabled}
         rows={rows}
         maxLength={max}
+        onInput={onInput}
       />
     );
   }
 }
+ */
