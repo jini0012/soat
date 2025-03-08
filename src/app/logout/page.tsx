a7f6a900102ad515ba06a07bace4e15722dde683
김지훈 <kim@jihun.io>
2025-03-09 04:50:03 +0900

    [add] 로그아웃 페이지에서 자동으로 홈으로 리다이렉트 기능 추가

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -2,10 +2,13 @@
 
 import { useEffect } from "react";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function LogoutPage() {
+  const router = useRouter();
   useEffect(() => {
     signOut();
+    router.push("/");
   }, []);
 
   return null;
