9ab67621f44a1d4f091e0566a7a71e860c081f90
HwiGyoung Lee <ter0000@naver.com>
2025-03-10 18:59:21 +0900

    [add] : indexedDB의 image store 추가 및 crud 기능 추가

---
+++ src/services/indexedDBService.ts
@@ -0,0 +1,50 @@
+import { openDB } from "idb";
+import { ImageItem } from "@/types/file";
+
+interface soatDB {
+  images: {
+    key: number;
+    value: ImageItem;
+    indexes: { "by-title": string; "by-date": number };
+  };
+  //여기에 필요한 스토어 인터페이스 작성
+}
+
+const dbName = "soat";
+const dbVersion = 1;
+
+export const initDB = async () => {
+  return openDB<soatDB>(dbName, dbVersion, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains("images")) {
+        db.createObjectStore("images", { keyPath: "id" });
+      }
+      //여기에 필요한 스토어 추가 가능
+    },
+  });
+};
+
+export const saveImage = async (item: ImageItem): Promise<IDBValidKey> => {
+  const db = await initDB();
+  return db.add("images", item);
+};
+
+export const getImage = async (id: number): Promise<ImageItem> => {
+  const db = await initDB();
+  return db.get("images", id);
+};
+
+export const getAllImages = async (): Promise<ImageItem[]> => {
+  const db = await initDB();
+  return db.getAll("images");
+};
+
+export const deleteImage = async (id: number): Promise<void> => {
+  const db = await initDB();
+  return db.delete("images", id);
+};
+
+export const clearAllImages = async (): Promise<void> => {
+  const db = await initDB();
+  return db.clear("images");
+};
