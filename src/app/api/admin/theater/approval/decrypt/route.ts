c3cf659b4b82cdac2abdc0be2da1570f935861e0
김지훈 <kim@jihun.io>
2025-03-11 12:26:57 +0900

    [add] 관리자 API에 계좌 이미지 복호화 기능 추가

---
+++ src/app/api/admin/theater/approval/decrypt/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { adminDb, adminStorage } from "@/app/api/firebaseAdmin";
+import NextCrypto from "next-crypto";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+const SECRET_KEY = process.env.NEXT_CRYPTO_SECRET_KEY;
+
+if (!SECRET_KEY) {
+  throw new Error("NEXT_CRYPTO_SECRET_KEY 환경 변수가 설정되지 않았습니다.");
+}
+
+const crypto = new NextCrypto(SECRET_KEY);
+
+// 암호화된 계좌 이미지를 복호화하여 base64로 반환
+export async function POST(request: NextRequest) {
+  const { accountImageUrl, originalImageType } = await request.json();
+
+  try {
+    const session = await getServerSession(authOptions);
+
+    // 인증되지 않은 요청일 경우 에러 반환
+    // 주의!!! admin 계정 개발이 완료되면 여기에 조건을 추가해야 합니다.
+    if (!session || !session.user) {
+      return NextResponse.json(
+        { error: "인증되지 않은 요청입니다." },
+        { status: 401 }
+      );
+    }
+
+    const accountImageRef = adminStorage.bucket().file(accountImageUrl);
+    const [content] = await accountImageRef.download();
+
+    const decryptedContent = await crypto.decrypt(content.toString());
+
+    return NextResponse.json({
+      base64Image: `data:image/${originalImageType};base64,${decryptedContent}`,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "계좌 이미지를 복호화하는데 실패했습니다." },
+      { status: 500 }
+    );
+  }
+}
