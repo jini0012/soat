41b64d5048cba884fe0189da3d35df8a76f633ac
jini0012 <jinhui.ha1208@gmail.com>
2025-03-20 12:07:50 +0900

    [edit] 검색 결과 항목 컴포넌트 수정 : API에서 가져온 데이터 구조에 맞게 props 변경 및 링크 추가

--- src/components/search/SearchResultItem.tsx
+++ src/components/search/SearchResultItem.tsx
@@ -1,40 +1,45 @@
-interface SearchResultItemProps {
-  // id: number;
-  imgUrl: string;
-  title: string;
-  venue: string;
-  date: string;
-  commentCount: number;
-  price: number;
-  saleStatus: string;
+import Link from "next/link";
+import { PerformanceData } from "@/app/api/performance/route";
+interface Item extends PerformanceData {
+  ratingSum: number;
 }
-export default function SearchResultItem({
-  imgUrl,
-  title,
-  venue,
-  date,
-  commentCount,
-  price,
-  saleStatus,
-}: SearchResultItemProps) {
+
+export default function SearchResultItem({ item }: { item: Item }) {
+  const {
+    id,
+    poster,
+    title,
+    bookingStartDate,
+    bookingEndDate,
+    price,
+    sellerTeam,
+    ratingSum,
+  } = item;
+  const saleStatus =
+    new Date() < new Date(bookingStartDate)
+      ? "판매예정"
+      : new Date() < new Date(bookingEndDate)
+      ? "판매중"
+      : "판매종료";
   const isSalePending = saleStatus === "판매예정";
 
   return (
     <li>
-      <article className="py-[20px] flex items-center border-b border-b-gray-300 md:gap-4">
-        {/* 공연 포스터 */}
-        <img
-          src={imgUrl}
-          alt="공연포스터"
-          className="w-[113px] h-[143px] cursor-pointer"
-        />
+      <Link href={`/detail/${id}`}>
+        <article className="py-[20px] flex items-center border-b border-b-gray-300 md:gap-4">
+          {/* 공연 포스터 */}
+          <img
+            src={poster.url}
+            alt={poster.fileName}
+            className="w-[113px] h-[143px] cursor-pointer"
+          />
 
-        {/* 공연 정보 */}
-        <div className="ml-4 flex-1">
-          {/* 판매 상태 표시 */}
-          <div className="flex items-center gap-[7px] text-[10px]">
-            <div
-              className={`border border-flesh-200 px-1 py-[0.7px] rounded-sm font-semibold 
+          {/* 공연 정보 */}
+          <div className="ml-4 flex-1">
+            {/* 판매 상태 표시 */}
+            <div className="flex items-center gap-[7px] text-[10px]">
+              <div
+                className={`border border-flesh-200 px-1 py-[0.7px] rounded-sm font-semibold 
                 ${
                   saleStatus === "판매예정"
                     ? "bg-gray-300 text-gray-500 border-gray-300"
@@ -46,52 +51,55 @@ export default function SearchResultItem({
                     ? "bg-flesh-100 text-flesh-500 border-flesh-100"
                     : ""
                 }`}
-            >
-              {saleStatus}
+              >
+                {saleStatus}
+              </div>
             </div>
-          </div>
 
-          {/* 공연 제목 */}
-          <h3 className="font-semibold text-base mt-1 mb-[0.8px] cursor-pointer">
-            {title}
-          </h3>
-          {/* 소극장 이름 */}
-          <p className="text-xs font-light mb-[0.8px] md:mb-[0.5px]">{venue}</p>
-
-          {/* 날짜 & 예매가 (데스크탑에서 가로 정렬) */}
-          <div className="text-xs text-gray-400 font-light md:flex md:items-center md:justify-between md:mb-[4px]">
-            <div className="md:flex-1 md:text-center md:text-sm">{date}</div>
-            <p className="hidden md:block text-flesh-600 font-semibold md:text-base ">
-              예매가{" "}
-              <span className="text-black"> {price.toLocaleString()}원</span>
+            {/* 공연 제목 */}
+            <h3 className="font-semibold text-base mt-1 mb-[0.8px] cursor-pointer">
+              {title}
+            </h3>
+            {/* 소극장 이름 */}
+            <p className="text-xs font-light mb-[0.8px] md:mb-[0.5px]">
+              {sellerTeam}
             </p>
-          </div>
 
-          {/* 한줄평 (판매예정과 판매종료일 때는 보이지 않음) */}
-          <p
-            className={`text-xs mt-2 mb-2 font-light pl-4 bg-star-icon bg-no-repeat bg-left md:mt-1 md:mb-1 
+            {/* 날짜 & 예매가 (데스크탑에서 가로 정렬) */}
+            <div className="text-xs text-gray-400 font-light md:flex md:items-center md:justify-between md:mb-[4px]">
+              <div className="md:flex-1 md:text-sm">{bookingEndDate}</div>
+              <p className="hidden md:block text-flesh-600 font-semibold md:text-base ">
+                예매가
+                <span className="text-black"> {price.toLocaleString()}원</span>
+              </p>
+            </div>
+
+            {/* 한줄평 (판매예정과 판매종료일 때는 보이지 않음) */}
+            <p
+              className={`text-xs mt-2 mb-2 font-light pl-4 bg-star-icon bg-no-repeat bg-left md:mt-1 md:mb-1 
     ${
       saleStatus === "판매예정" || saleStatus === "판매종료" ? "invisible" : ""
     }`}
-          >
-            한줄평({commentCount})
-          </p>
+            >
+              한줄평({ratingSum})
+            </p>
 
-          {/* 예매 버튼 */}
-          <button
-            className={`w-[65px] h-[25px] rounded-md text-white font-medium text-xs ${
-              saleStatus === "판매종료"
-                ? "invisible"
-                : isSalePending
-                ? "bg-gray-300 cursor-not-allowed"
-                : "bg-flesh-500 active:bg-flesh-600"
-            }`}
-            disabled={isSalePending || saleStatus === "판매종료"}
-          >
-            예매하기
-          </button>
-        </div>
-      </article>
+            {/* 예매 버튼 */}
+            <button
+              className={`w-[65px] h-[25px] rounded-md text-white font-medium text-xs ${
+                saleStatus === "판매종료"
+                  ? "invisible"
+                  : isSalePending
+                  ? "bg-gray-300 cursor-not-allowed"
+                  : "bg-flesh-500 active:bg-flesh-600"
+              }`}
+              disabled={isSalePending || saleStatus === "판매종료"}
+            >
+              예매하기
+            </button>
+          </div>
+        </article>
+      </Link>
     </li>
   );
 }
