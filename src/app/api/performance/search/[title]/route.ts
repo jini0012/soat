d8dc687819effeb1158e06b326b08be54f470e26
jini0012 <jinhui.ha1208@gmail.com>
2025-03-20 09:22:37 +0900

    [add] 검색 API 추가 : 공연 title로 조회하는 기능 구현

---
+++ src/app/api/performance/search/[title]/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import { adminDb } from "@/app/api/firebaseAdmin";
+import { PerformanceData } from "@/app/api/performance/route";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { title: string } }
+) {
+  const { title } = params;
+
+  console.log("검색어:", title); // 검색어 로그 출력
+
+  if (!title || title.trim() === "") {
+    return NextResponse.json(
+      { message: "검색 정보를 가져올 수 없습니다." },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const performancesRef = adminDb.collection("performances");
+
+    const snapshot = await performancesRef
+      .where("title", ">=", title)
+      .where("title", "<=", title + "\uf8ff")
+      .get();
+
+    if (snapshot.empty) {
+      return NextResponse.json(
+        { message: "검색 결과가 없습니다." },
+        { status: 404 }
+      );
+    }
+
+    const performances: PerformanceData[] = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as PerformanceData[];
+    return NextResponse.json(performances);
+  } catch (error) {
+    console.error("검색 오류 :", error);
+    return NextResponse.json(
+      { message: "검색 중 오류가 발생했습니다." },
+      { status: 500 }
+    );
+  }
+}
