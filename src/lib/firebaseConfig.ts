bdae0979995120aa71f5fab307673a62644c31fc
김지훈 <kim@jihun.io>
2025-03-26 15:32:59 +0900

    [add] Firebase 초기화 및 Firestore 인스턴스 생성 코드 추가

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { FirebaseOptions } from "firebase/app";
+
+interface FirebaseConfigType {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+
+const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG;
+
+if (!firebaseConfig) {
+  throw new Error("Firebase 설정이 필요합니다.");
+}
+
+console.log(firebaseConfig);
+
+const parsedFirebaseConfig = JSON.parse(firebaseConfig);
+
+console.log(parsedFirebaseConfig);
+
+// Firebase 앱 초기화
+const app = initializeApp(parsedFirebaseConfig as FirebaseOptions);
+
+// Firestore 인스턴스 생성
+const db = getFirestore(app);
+
+export { db };
