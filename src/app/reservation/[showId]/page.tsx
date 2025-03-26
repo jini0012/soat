e3e665f0c9bdb9dc57d3dccc81b8afb1c63f6a2a
김지훈 <kim@jihun.io>
2025-03-26 15:34:15 +0900

    [add] 공연 정보 불러오기 기능 추가 및 예약 팝업에 공연 데이터 전달

--- src/app/reservation/[showId]/page.tsx
+++ src/app/reservation/[showId]/page.tsx
@@ -1,20 +1,46 @@
 import BookHeader from "@/components/booking/BookHeader";
 import BookMain from "@/components/booking/BookMain";
+import { adminDb } from "@/app/api/firebaseAdmin";
+import { PerformanceData } from "@/app/api/performance/route";
 
-export default function ReservationPopup({
+export default async function ReservationPopup({
   params,
 }: {
   params: { showId: string };
 }) {
   const { showId } = params;
 
-  // 테스트 용 코드
-  let showName = showId === "test" ? "2024 주인님 단독 공연" : showId;
+  let showName = "공연 정보 없음";
+  let performanceDoc = {} as PerformanceData;
+
+  try {
+    const showDoc = await adminDb.collection("performances").doc(showId).get();
+
+    if (!showDoc.exists) {
+      throw new Error("해당 공연이 존재하지 않습니다.");
+    }
+
+    const showData = showDoc.data();
+
+    if (!showData) {
+      throw new Error("공연 정보를 불러올 수 없습니다.");
+    }
+
+    showName = showData.title || "공연 정보 없음";
+    performanceDoc = {
+      ...showData,
+      createdAt: showData.createdAt.toDate(),
+      updatedAt: showData.updatedAt.toDate(),
+    } as PerformanceData;
+  } catch (error) {
+    console.error("공연 정보 불러오기 실패:", error);
+    return;
+  }
 
   return (
     <>
       <BookHeader showName={showName} />
-      <BookMain showId={showId} />
+      <BookMain showId={showId} performanceData={performanceDoc} />
     </>
   );
 }
