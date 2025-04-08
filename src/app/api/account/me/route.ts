e8420e5e4b7c8f4cb8d7f8abf4dc278447edd573
jini0012 <jinhui.ha1208@gmail.com>
2025-04-08 18:04:46 +0900

    [edit] authOptions.ts 파일 분리 및 authOptions import 경로 수정

--- src/app/api/account/me/route.ts
+++ src/app/api/account/me/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/auth/authOptions";
 import { adminDb } from "../../firebaseAdmin";
 
 export async function GET(request: NextRequest) {
--- src/app/api/manager/performance/route.ts
+++ src/app/api/manager/performance/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { adminDb } from "@/app/api/firebaseAdmin";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/auth/authOptions";
 import { create } from "domain";
 
 export async function GET(request: NextRequest) {
