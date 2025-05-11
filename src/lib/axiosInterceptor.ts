a1a5ba7381e39723b23fd9085a53a0f8d7d8b9a3
jini0012 <jinhui.ha1208@gmail.com>
2025-05-11 03:56:49 +0000

    [fix] axiosInterceptor에서 세션 쿠키 이름을 환경에 따라 동적으로 설정하도록 수정

--- src/lib/axiosInterceptor.ts
+++ src/lib/axiosInterceptor.ts
@@ -2,10 +2,15 @@ import axios from "axios";
 import { cookies } from "next/headers";
 
 export default async function axiosInterceptor(url: string) {
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const sessionCookieName = isDevelopment
+    ? "next-auth.session-token"
+    : "__Secure-next-auth.session-token";
+
   const sessionToken = cookies().get("next-auth.session-token")?.value;
   const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
     headers: {
-      Cookie: `next-auth.session-token=${sessionToken}`,
+      Cookie: `${sessionCookieName}=${sessionToken}`,
     },
   });
 
